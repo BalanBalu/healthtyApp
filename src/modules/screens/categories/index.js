560b048ef1573cf990ba4407cd141239fbbdbd11
Ayyappa <arul@alldirectionsource.com>
2019-06-07 16:16:49 +0530

    category defect fixed

--- src/modules/screens/categories/index.js
+++ src/modules/screens/categories/index.js
@@ -5,55 +5,62 @@ import { messageShow, messageHide } from '../../providers/common/common.action';
 import LinearGradient from 'react-native-linear-gradient';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { connect } from 'react-redux'
-import { StyleSheet, Image, TouchableOpacity, View ,FlatList} from 'react-native';
+import { StyleSheet, Image, TouchableOpacity, View, FlatList } from 'react-native';
+import { catagries } from '../../providers/catagries/catagries.actions';
+
 
 class Categories extends Component {
   constructor(props) {
     super(props)
-    this.state={
-      data:this.props.navigation.state.params.data,
-     
-    }
-
+    this.state = {
+      data: this.props.navigation.state.params.data,
+         }
   }
+  componentDidMount() {
+    console.log(' this.state.data' + JSON.stringify(this.state.data));
+  }
+
   navigateToCategorySearch(categoryName) {
+    console.log(categoryName);
     let serachInputvalues = [{
-         type: 'category',
-         value: [ categoryName ]
-     }]      
-     this.props.navigation.navigate('Doctor List', { resultData: serachInputvalues })  
+      type: 'category',
+      value: [categoryName]
+
+    }]
+    this.props.navigation.navigate('Doctor List', { resultData: serachInputvalues })
   }
+
   render() {
-
-
+    const { user: { isLoading } } = this.props;
     return (
-
       <Container style={styles.container}>
-       
         <Content style={styles.bodyContent}>
-       
-
-        <FlatList horizontal={false} numColumns={3}
-                                        data={this.state.data}
-                                        extraData={this.state.data}
-                                        renderItem = {({item, index}) =>
-          <Grid>
-            
-              <Item style={styles.column} onPress={()=> this.navigateToCategorySearch(item.category_name)}>
+          <FlatList horizontal={false} numColumns={3}
+            data={this.state.data}
+            extraData={this.state}
+            renderItem={({ item, index }) =>
+            <Grid>
+              <Item>
+              {isLoading ? <Spinner color='blue' /> : null}
+              <TouchableOpacity onPress={() => this.navigateToCategorySearch(item.category_name)}>
+                
               <Col>
-              <LinearGradient
-                  colors={['#7357A2', '#62BFE4']} style={{ borderRadius: 10, padding: 10, height:100, width:100, marginLeft: 'auto', marginRight: 'auto' }}>
-                  <Image source={{ uri: 'http://pluspng.com/img-png/orthopedics-png--350.png' }} style={styles.customImage} />
-                </LinearGradient>
+                  <LinearGradient
+                    colors={['#7357A2', '#62BFE4']} style={{ borderRadius: 10, padding: 10, height: 100, width: 100, marginLeft: 'auto', marginRight: 'auto' }}>
+                    <Image
+                      // source={{ uri: item.imageBaseURL + '/' + item.category_id + '.png' }} style={styles.customImage} 
+                      source={{ uri: 'http://pluspng.com/img-png/orthopedics-png--350.png' }} style={styles.customImage}
+                    />
+                  </LinearGradient>
 
-                <Text style={styles.titleText}>{item.category_name}</Text>
+                  <Text style={styles.titleText}>{item.category_name}</Text>
+
                 </Col>
-               </Item>
-              
-
-          </Grid>
-          }
-          keyExtractor={(item, index) => index.toString()}
+              </TouchableOpacity>
+              </Item>
+              </Grid>
+            }
+            keyExtractor={(item, index) => index.toString()}
           />
         </Content>
       </Container>
