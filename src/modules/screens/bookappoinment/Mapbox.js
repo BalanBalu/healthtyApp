9c69e44a64bdc04454f4c6773560e8d69dee6e94
Anish <anishkishore772@gmail.com>
2019-06-10 21:56:10 +0800

    mapbox displaying coordinates

--- src/modules/screens/bookappoinment/Mapbox.js
+++ src/modules/screens/bookappoinment/Mapbox.js
@@ -1,36 +1,52 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Container, Content,Card,Text } from 'native-base';
-import {  View,Image } from 'react-native';
-
+import { Container, Content, Card, Text } from 'native-base';
+import { View, Image } from 'react-native';
 import MapboxGL from '@react-native-mapbox-gl/maps';
+import { Directions } from 'react-native-gesture-handler';
+
+
 let token = 'pk.eyJ1IjoiYnJpdmluc3JlZSIsImEiOiJjanc2Y3hkZHcxOGhvNDVwOXRhMWo2aDR1In0.EV8iYtfMxEcRcn8HcZ0ZPA';
+
 MapboxGL.setAccessToken(token);
 class Mapbox extends Component {
-    constructor(props) {
-      super(props)
-    }
+  constructor(props) {
+    super(props)
+  }
 
-    
-render() {
-  const { navigation } = this.props;
-  const data = navigation.getParam('coordinates');   
-  console.log(data);
-  return(
-    <Container>
-      <Content contentContainerStyle={{flex:1}}>
-        <View style={{ flex: 1 }}>
+
+  render() {
+    const { navigation } = this.props;
+    const data = navigation.getParam('coordinates');
+    //data.location.location.coordinates = [13.067439, 80.237617]; //chennai location
+    console.log('data' + JSON.stringify(data));
+    var hospitaldestination = {
+      lat: data.location.location.coordinates[1],
+      lon: data.location.location.coordinates[0]
+    }
+    console.log(hospitaldestination.lat + 'coordinates lat');
+    console.log(hospitaldestination.lon + 'coordinates lon');
+
+    return (
+      <Container>
+        <Content contentContainerStyle={{ flex: 1 }}>
+          <View style={{ flex: 1 }}>
             <MapboxGL.MapView
               ref={c => (this._map = c)}
               zoomLevel={25}
               zoomEnabled={true}
               showUserLocation={false}
-              centerCoordinate={data.location.location.coordinates}
+              centerCoordinate={[hospitaldestination.lat, hospitaldestination.lon]}
               style={{ flex: 1 }}
               styleURL={MapboxGL.StyleURL.Light}>
+              
+              <Directions>
+                
+              </Directions>
+
               <MapboxGL.PointAnnotation
                 id='Pin'
-                coordinate={data.location.location.coordinates}>
+                coordinate={[hospitaldestination.lat, hospitaldestination.lon]}>
                 <Image
                   style={{
                     flex: 1,
@@ -42,19 +58,19 @@ render() {
             </MapboxGL.MapView>
           </View>
           <Card>
-          <Text style={{ borderRadius:10,color:'gray',paddingTop:15}}>Location details</Text>
+            <Text style={{ borderRadius: 10, color: 'gray', paddingTop: 15 }}>Location details</Text>
 
           </Card>
 
- 
 
 
 
-      </Content>
-    </Container>
 
-  )
-}
+        </Content>
+      </Container>
+
+    )
+  }
 }
 
 
@@ -67,9 +83,8 @@ render() {
 
 function loginState(state) {
 
-    return {
-      user: state.user
-    }
+  return {
+    user: state.user
   }
-  export default connect(loginState)(Mapbox)
-  
+}
+export default connect(loginState)(Mapbox)
