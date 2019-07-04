db79af56e950b0dd0a20643a8d64e4ec592c11e7
yuvaraj <yuvaraj@alldirectionsource.com>
2019-07-04 11:31:10 +0530

    action file added

--- src/modules/providers/bookappointment/bookappointment.action.js
+++ src/modules/providers/bookappointment/bookappointment.action.js
@@ -249,38 +249,3 @@ export const getPatientWishList = async (userId) => {
 }
 
 
-/* Get Patient Notification List  */
-export const fetchUserNotification = async (userId) => {
-  try {
-   
-    let endPoint = '/notifications/' + userId ;
-    let response = await getService(endPoint);
-    let respData = response.data;
-    return respData;
-  } catch (e) {
-    console.log(e.message);
-    return {
-      message: 'exception' + e,
-      success: false
-    }
-  }
-}
-// put services
-export const UpDateUserNotification = async (updateNode, notificationIds) => {
-  try {
-
-    let endPoint = '/notifications/status/'+updateNode +'/'+notificationIds;
-    console.log(endPoint)
-    let response = await putService(endPoint);
-    let respData = response.data;
-    return respData;
-  } catch (e) {
-    console.log(e.message);
-    return {
-      message: 'exception' + e,
-      success: false
-    }
-  }
-}
-
-
