c20ce74dd4bda009c177205e7ffdfc6b146b6d29
Sathish Krishnan <sathiskrish20@gmail.com>
2019-07-09 08:30:28 +0530

    removed

--- src/setup/config.js
+++ src/setup/config.js
@@ -2,4 +2,4 @@ import {Platform} from 'react-native';
 export const IS_ANDROID = Platform.OS === 'android';
 export const IS_IOS = Platform.OS === 'ios';
 
-export const API_URL = 'http://10.12.0.204:8080/api/';
+export const API_URL = 'http://192.168.1.3:3200/api/';
