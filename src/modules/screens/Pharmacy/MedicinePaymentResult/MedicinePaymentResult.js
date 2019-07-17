c136c114e9ed891ccf7c302efd9637737b4b9c9b
Shobana <shobanarathnakumar@gmail.com>
2019-07-17 18:54:24 +0530

    Store and bind the datas and offers calculation

--- src/modules/screens/Pharmacy/MedicinePaymentResult/MedicinePaymentResult.js
+++ src/modules/screens/Pharmacy/MedicinePaymentResult/MedicinePaymentResult.js
@@ -2,150 +2,145 @@ import React, { Component } from 'react';
 import { Container, Content, Text, Title, Header, Form, Textarea, Button, H3, Item, List, ListItem, Card, Input, Left, Right, Thumbnail, Body, Icon, Footer, FooterTab, Picker, Segment, CheckBox, View, Badge } from 'native-base';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { StyleSheet, Image, AsyncStorage, TextInput, FlatList, TouchableOpacity } from 'react-native';
+import { Loader } from '../../../../components/ContentLoader';
 
 class MedicinePaymentResult extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            cartData:[]            
+            cart:[],
+            isLoading:true           
         }
     }
 
    async componentDidMount(){
-        const cartData = await AsyncStorage.getItem('cart');
-        console.log('cartData'+ cartData)
-        if(this.state.cartData == undefined)
-        {
-            this.state.cartData=[];
+     const cart=[
+            {
+                medicineName : 'Dolo650',
+                price : 100,
+                offerPercentage : 20,
+                quantity: 5
+            },
+            {
+                medicineName : 'Antibiotic',
+                price : 100,
+                offerPercentage : 50,
+                quantity: 10
+            }
+        ]
+       await AsyncStorage.setItem('cartItems', JSON.stringify(cart))
+       this.getAddToCart();
+    }
+
+    getAddToCart= async() => {
+    try{
+        const cartItems = await AsyncStorage.getItem('cartItems');
+        if( cartItems === undefined){
+            this.setState({ cart: [], isLoading: false });
+        }else{       
+            this.setState({ cart: JSON.parse(cartItems), isLoading: false });
         }
-        this.setState({ 
-             cartData:[{
-                medicineId: '5d2c2005df5b14242999add7',
-                medicineName: 'Dolo650',
-                quantity: 5,
-                pharmacyName: 'MedPlus',
-                price: 80 
-             },
-             {
-                medicineId: '5d2d89157362ff7b86599eff  ',
-                medicineName: 'Antibiotic',
-                quantity: 10,
-                pharmacyName: 'Apollo',
-                price: 50 
-             }
-        ]})
-        console.log('cartData'+ JSON.stringify(this.state.cartData))
+        console.log( 'cart' + this.state.cart )
+    }
+    catch(e){
+        console.log(e);
+    }
     }
     increase =async(index)=>{
-        let addQuantity= this.state.cartData;
+        let addQuantity= this.state.cart;
         addQuantity[index].quantity++;
         console.log(addQuantity)
         await this.setState({quantity: addQuantity})
-
     }
     decrease =async(index)=>{
-        if(this.state.cartData.quantity != 0){
-        let minusQuantity= this.state.cartData;
-        minusQuantity[index].quantity--;
+        if(this.state.cart.quantity !== null){
+        let minusQuantity= this.state.cart;
         console.log(minusQuantity)
+        minusQuantity[index].quantity--;
         await this.setState({quantity: minusQuantity})
         }
     }
-    render() {
-            const{ cartData}= this.state
+    medicineOffer(item){
 
-        return (
+        return parseInt(item.price) - ((parseInt(item.offerPercentage)/100) * parseInt(item.price));
+    }
+  
+    render() {            
+              const {isLoading,cart} = this.state;
+              console.log(this.state.cart)
+     return (            
+      <Container style={styles.container}>
 
-            <Container style={styles.container}>
+          {isLoading == true ? <Loader style='list' /> :
 
-                <Content style={styles.bodyContent}>
+             <Content style={styles.bodyContent}>
 
-                    <Grid style={styles.curvedGrid}>
+             <Grid style={styles.curvedGrid}>
+              </Grid>
+             <Grid style={{ marginTop: -60, height: 100, }}>
+               <Row style={{ justifyContent: 'center' }}>
+                  <Text style={{ fontFamily: 'OpenSans', color: '#fff', fontSize: 18, }}>CHECKOUT</Text>
+               </Row>
+             </Grid>
 
-                    </Grid>
-                    <Grid style={{ marginTop: -60, height: 100, }}>
-                        <Row style={{ justifyContent: 'center' }}>
+            <Card transparent >
+            <Grid >
+              <Row style={{ justifyContent: 'center', width: '100%', marginTop: -15 }}>
+                <Text style={{ fontFamily: 'OpenSans', fontWeight: 'bold', fontSize: 20, padding: 5 }}>Your Order</Text>
+               </Row>
+            </Grid>
 
-                            <Text style={{ fontFamily: 'OpenSans', color: '#fff', fontSize: 18, }}>CHECKOUT</Text>
+                <FlatList
+                   data={cart}
+                   extraData={this.state}
+                   keyExtractor={(item, index) => index.toString()}
+                   renderItem={({ item, index }) =>
 
+                <Card style={{ marginTop: 10, padding: 5, height: 180 }}>
+                  <Grid>
+                    <Row >
+                      <Image source={{ uri: 'https://static01.nyt.com/images/2019/03/05/opinion/05Fixes-1/05Fixes-1-articleLarge.jpg?quality=75&auto=webp&disable=upscale' }} style={{
+                                        width: 100, height: 100, borderRadius: 10, marginTop: 20 }} />
 
-                        </Row>
-                    </Grid>
+                        <View style={{ width: '75%', }}>
+                            <Text style={styles.labelTop}>{item.medicineName} </Text>
+                        </View>
+                     </Row>
 
+                        <View style={{ marginLeft: 105, flex: 1, flexDirection: 'row', marginTop: 15 }}>
+                          <Row>
+                            <Text style={styles.subText}>{'\u20B9'}{this.medicineOffer(item)}</Text>
+                            <Text style={{ marginLeft: 10, marginTop: 2, color: 'gray', fontSize: 15, textDecorationLine: 'line-through', textDecorationStyle: 'solid', textDecorationColor: 'gray' }}>
+                                            {'\u20B9'}{item.price}</Text>
+                          </Row>
+                            <Text style={{ fontFamily: 'OpenSans', fontSize: 15, color: '#ffa723', marginLeft: 10, fontWeight: 'bold' }}> {'Get'+ ' ' +item.offerPercentage+ ' ' +'Off'}</Text>
+                        </View>
+                                
+                        <View style={{ flex: 1, flexDirection: 'row', marginLeft: 110 }}>
+                          <Button style={{ padding: 0, justifyContent: 'center', borderWidth: 1, borderColor: '#c26c57', width: 30, height: 25, backgroundColor: 'white'}} onPress={()=>this.decrease(index)}>
+                            <Text style={{ fontSize: 40, textAlign: 'center', marginTop: -5, color: '#c26c57' }}> - </Text>
+                          </Button>
 
-                    {/* <Grid style={{ marginTop: -70, height: 100, }}>
-                        <Row style={{ marginLeft: 100, }}>
-                            <Col>
-                                <Text style={{ fontFamily: 'OpenSans', color: '#fff', fontSize: 14 }}>Date: </Text>
-                            </Col>
-                            <Col>
-                                <Text style={{ fontFamily: 'OpenSans', color: '#fff', fontSize: 14 }}> 24,2019</Text>
-                            </Col>
-                        </Row>
-                    </Grid> */}
-                    <Card transparent >
-                        <Grid >
-                            <Row style={{ justifyContent: 'center', width: '100%', marginTop: -15 }}>
-                                <Text style={{ fontFamily: 'OpenSans', fontWeight: 'bold', fontSize: 20, padding: 5 }}>Your Order</Text>
-                            </Row>
+                         <View>
+                            <TextInput style={{ marginLeft: 5, color: '#c26c57' }}>{item.quantity}</TextInput>
+                         </View>
 
-                        </Grid>
-                        <FlatList
-                        data={cartData}
-                        extraData={this.state}
-                        renderItem = {({item, index}) =>  
+                          <Button style={{ padding: 0, justifyContent: 'center', borderWidth: 1, borderColor: '#c26c57', width: 30, height: 25, marginLeft: 5, backgroundColor: 'white' }} onPress={()=>this.increase(index)}>
+                             <Text style={{ fontSize: 20, textAlign: 'center', marginTop: -5, color: '#c26c57' }}>+</Text>
+                          </Button>
 
-                        <Card style={{ marginTop: 10, padding: 5, height: 180 }}>
-                            <Grid>
-                                <Row >
-                                    <Image source={{ uri: 'https://static01.nyt.com/images/2019/03/05/opinion/05Fixes-1/05Fixes-1-articleLarge.jpg?quality=75&auto=webp&disable=upscale' }} style={{
-                                        width: 100, height: 100, borderRadius: 10,
-                                        marginTop: 20
-                                    }} />
-                                    <View style={{ width: '75%', }}>
-                                        <Text style={styles.labelTop}>{item.medicineName} </Text>
+                        <TouchableOpacity>
+                            <Icon style={{ fontSize: 30, color: 'red', marginLeft: 2, marginLeft: 50, marginTop: -4 }} name='ios-trash' />
+                        </TouchableOpacity>
 
-                                    </View>
+                    </View>
+                  </Grid>
+                </Card>
+                }/>
 
-                                </Row>
-                                <View style={{ marginLeft: 105, flex: 1, flexDirection: 'row', marginTop: 15 }}>
-                                       <Row>
-                                        <Text style={styles.subText}>{'\u20B9'}{item.price}</Text>
-                                        <Text style={{ marginLeft: 10, marginTop: 2, color: 'gray', fontSize: 15, textDecorationLine: 'line-through', textDecorationStyle: 'solid', textDecorationColor: 'gray' }}>
-                                            {'\u20B9'}100</Text>
-                                    </Row>
-                                    <Text style={{ fontFamily: 'OpenSans', fontSize: 15, color: '#ffa723', marginLeft: 10, fontWeight: 'bold' }}> Get 50% Off</Text>
-                                </View>
+            </Card>
+            </Content>}
 
-                                <View style={{ flex: 1, flexDirection: 'row', width: '65%', marginLeft: 110, }}>
-                                    <Text style={{ fontSize: 14, color: '#000', marginTop: -10 }}>{item.pharmacyName} </Text>
-
-                                </View>
-
-                                <View style={{ flex: 1, flexDirection: 'row', marginLeft: 110 }}>
-                                    <Button style={{ padding: 0, justifyContent: 'center', borderWidth: 1, borderColor: '#c26c57', width: 30, height: 25, backgroundColor: 'white'}} onPress={()=>this.decrease(index)}>
-                                        <Text style={{ fontSize: 40, textAlign: 'center', marginTop: -5, color: '#c26c57' }}> - </Text>
-                                    </Button>
-                                    <View>
-                                        <TextInput style={{ marginLeft: 5, color: '#c26c57' }}>{item.quantity}</TextInput>
-                                    </View>
-                                    <Button style={{ padding: 0, justifyContent: 'center', borderWidth: 1, borderColor: '#c26c57', width: 30, height: 25, marginLeft: 5, backgroundColor: 'white' }} onPress={(index)=>this.increase()}>
-                                        <Text style={{
-                                            fontSize: 20, textAlign: 'center', marginTop: -5, color: '#c26c57'
-                                        }}>+</Text>
-                                    </Button>
-                                     <TouchableOpacity>
-                                        <Icon style={{ fontSize: 30, color: 'red', marginLeft: 2, marginLeft: 50, marginTop: -4 }} name='ios-trash' />
-                                    </TouchableOpacity>
-                                </View>
-
-
-                            </Grid>
-                        </Card>
-                        }>
-                        </FlatList>
-                    </Card>
-                </Content>
                 <Footer style={{ backgroundColor: '#7E49C3', }}>
                     <Row style={{ justifyContent: 'center', marginTop: 15 }}>
                         <Text style={{ fontSize: 20, fontWeight: 'bold', color: '#fff' }}>Total </Text>
@@ -158,10 +153,8 @@ class MedicinePaymentResult extends Component {
                     </Col>
                 </Footer>
             </Container >
-
         )
     }
-
 }
 
 
@@ -233,20 +226,14 @@ const styles = StyleSheet.create({
         fontSize: 14,
         fontWeight: 'bold',
         marginTop: 5
-
-
     },
     medPhar:
     {
         fontFamily: 'OpenSans',
         fontSize: 12,
-
-
-
     },
     transparentLabel:
     {
-
         borderBottomColor: 'transparent',
         backgroundColor: '#F1F1F1',
         height: 45,
@@ -265,7 +252,6 @@ const styles = StyleSheet.create({
         color: '#fff',
         paddingLeft: 10,
         paddingTop: 5
-
     },
     button1: {
         backgroundColor: "#5cb75d",
@@ -274,12 +260,8 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         justifyContent: 'center',
         padding: 5,
-
-
-
     },
     button2: {
-
         marginLeft: 70,
         marginTop: -5,
         borderRadius: 10,
@@ -287,9 +269,6 @@ const styles = StyleSheet.create({
         padding: 5,
         height: 35,
         backgroundColor: 'gray',
-
-
-
     },
     subText: {
         fontFamily: 'OpenSans',
