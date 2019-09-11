5473fc08b0848b406406e07524b0ea1af592d72c
yuvaraj <yuvaraj@alldirectionsource.com>
2019-09-11 15:33:15 +0530

    time issue fix

--- src/modules/screens/Reviews/InsertReview.js
+++ src/modules/screens/Reviews/InsertReview.js
@@ -44,7 +44,7 @@ class InsertReview extends Component {
     let doctorId = reviewData.doctor_id;
     let appointmentId = reviewData._id;
     await this.setState({ userId: userId, doctorId: doctorId, appointmentId: appointmentId, data: reviewData });
-    
+
   }
 
   updateAppointmentStatus = async (data, updatedStatus) => {
@@ -61,7 +61,7 @@ class InsertReview extends Component {
 
       let userId = await AsyncStorage.getItem('userId');
       let result = await appointmentStatusUpdate(data.doctor_id, data._id, requestData);
-      
+
     } catch (e) {
       console.log(e);
     }
@@ -71,7 +71,7 @@ class InsertReview extends Component {
     try {
       let userId = this.state.data.user_id;
       let overallrating = (this.state.cleanness_rating + this.state.staff_rating + this.state.wait_time_rating) / 3;
-     
+
       if (this.state.comments != null) {
         let insertReviewData = {
           user_id: userId,
@@ -85,17 +85,17 @@ class InsertReview extends Component {
           comments: this.state.comments,
           is_doctor_recommended: this.state.doctorRecommended,
         };
-        
+
         let result = await addReview(userId, insertReviewData);
-       
+
 
         if (result.success) {
-          
+
           // this.state.data.appointment_status = 'COMPLETED';
           // await this.updateAppointmentStatus(this.state.data, 'COMPLETED')
-         
+
           this.props.navigation.pop();
-          
+
         }
       } else {
         Toast.show({
@@ -134,16 +134,16 @@ class InsertReview extends Component {
             <Card>
               <CardItem style={styles.text}>
                 <Body>
-                  <Text > How was your visit with {(data && data.prefix != undefined ? data && data.prefix:' ') + (data && data.doctorInfo.first_name) + " " + (data && data.doctorInfo.last_name)} ? help other patients by leaving a Review </Text>
+                  <Text > How was your visit with {(data && data.prefix != undefined ? data && data.prefix : '') + (data && data.doctorInfo.first_name) + " " + (data && data.doctorInfo.last_name)} ? help other patients by leaving a Review </Text>
                 </Body>
               </CardItem>
               <CardItem>
                 <Body>
                   <Text style={{ marginTop: 5, }}>
-                    <Text style={{ fontWeight: "bold" }}>
+                    Your appointment with {(data && data.prefix != undefined ? data && data.prefix : '') + (data && data.doctorInfo.first_name) + " " + (data && data.doctorInfo.last_name)} on <Text style={{ fontWeight: "bold" }}>
                       {formatDate(data.appointment_starttime, 'MMMM-DD-YYYY') + "   " +
-                        formatDate(data[0] && data[0].appointment_starttime, 'hh:mm A')}
-                    </Text> with {(data && data.prefix != undefined ? data && data.prefix : ' ') + (data && data.doctorInfo.first_name) + " " + (data && data.doctorInfo.last_name)}</Text>
+                        formatDate(data.appointment_starttime, 'hh:mm A')}
+                    </Text> </Text>
                   <Row style={{ marginTop: 20 }}>
                     <Text style={{ fontSize: 16 }}>Cleanliness</Text>
                     <StarRating fullStarColor='#FF9500' starSize={20} containerStyle={{ width: 110, marginLeft: 50 }}
