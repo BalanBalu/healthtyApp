94d015be023165069faeca2f139b2904b32b54cd
yuvaraj <yuvaraj@alldirectionsource.com>
2019-09-11 18:49:43 +0530

    fee added

--- src/modules/screens/MyAppointments/AppointmentDetails.js
+++ src/modules/screens/MyAppointments/AppointmentDetails.js
@@ -48,14 +48,14 @@ class AppointmentDetails extends Component {
       const appointmentId = navigation.getParam('appointmentId');
 
       await this.setState({ appointmentId: appointmentId });
-      
+
       await this.appointmentDetailsGetById()
     }
     else {
 
       let doctorId = appointmentData.doctor_id;
       let appointmentId = appointmentData._id;
-     
+
       await this.setState({
         doctorId: doctorId, appointmentId: appointmentId,
         userId: userId, data: appointmentData,
@@ -65,7 +65,7 @@ class AppointmentDetails extends Component {
         this.getDoctorDetails(),
         this.getUserReviews()
       ])
-     
+
     }
 
     await this.setState({ isLoading: false })
@@ -77,9 +77,10 @@ class AppointmentDetails extends Component {
 
       let fields = 'first_name,last_name,prefix,education,specialist,email,mobile_no,experience,hospital,language,professional_statement,fee,profile_image';
       let resultDetails = await bindDoctorDetails(this.state.doctorId, fields);
-
+      console.log('getdoctorDetails');
+      console.log(resultDetails.data)
       if (resultDetails.success) {
-        
+
         await this.setState({ doctorData: resultDetails.data });
 
         let experience = 'N/A';
@@ -150,11 +151,11 @@ class AppointmentDetails extends Component {
     if (result.success) {
 
       await new Promise.all([
-      this.setState({ doctorId: result.data[0].doctor_id, data: result.data[0] }),
-     
+        this.setState({ doctorId: result.data[0].doctor_id, data: result.data[0] }),
+
         this.getDoctorDetails()
       ])
-      
+
 
     }
 
@@ -171,8 +172,8 @@ class AppointmentDetails extends Component {
 
 
     const fromNotification = navigation.getParam('fromNotification');
-    
-    
+
+
     if (fromNotification == true || fromNotification != undefined) {
       let doctorInfo = {
         first_name: this.state.doctorData.first_name,
@@ -214,7 +215,7 @@ class AppointmentDetails extends Component {
           text: result.message,
           duration: 3000
         })
-      
+
 
         this.setState({ appointmentStatus: appointmentStatus, data: result });
       }
@@ -229,7 +230,7 @@ class AppointmentDetails extends Component {
     this.state.data.prefix = this.state.doctorData.prefix;
     const { navigation } = this.props;
     const fromNotification = navigation.getParam('fromNotification');
-    
+
     if (fromNotification == true || fromNotification != undefined) {
       let doctorInfo = {
         first_name: this.state.doctorData.first_name,
@@ -273,8 +274,9 @@ class AppointmentDetails extends Component {
                     <Thumbnail square source={renderProfileImage(doctorData)} style={{ height: 86, width: 86 }} />
                   </Left>
                   <Body>
-                    <Text style={{ fontSize: 15,fontFamily:'OpenSans',fontWeight:'bold' }}>{(doctorData && doctorData.prefix ? doctorData.prefix : '') +  + (doctorData && doctorData.first_name) + " " + (doctorData && doctorData.last_name)},
-                      <Text style={{ fontSize: 13,fontFamily:'OpenSans' }}>{education}</Text>
+
+                    <Text style={{ fontSize: 15, fontFamily: 'OpenSans', fontWeight: 'bold' }}>{doctorData && doctorData.prefix != undefined ? doctorData && doctorData.prefix : '' + + doctorData && doctorData.first_name + " " + doctorData && doctorData.last_name},
+                      <Text style={{ fontSize: 13, fontFamily: 'OpenSans' }}>{education}</Text>
 
                     </Text>
                     <Text note style={styles.customText}>{specialist} </Text>
@@ -284,11 +286,11 @@ class AppointmentDetails extends Component {
 
                 <Grid>
                   <Col style={{ backgroundColor: 'transparent', borderRightWidth: 0.5, borderRightColor: 'gray', justifyContent: 'center' }}>
-                    <Text style={styles.topValue}> {data.fee != undefined ? data.fee : 'N/A'} </Text>
-                    <Text note style={styles.bottomValue}> Hourly Rate </Text>
+                    <Text style={styles.topValue}> {data.fee != undefined && data.fee != 0? data.fee : 'N/A'} </Text>
+                    <Text note style={styles.bottomValue}> Fee </Text>
                   </Col>
                   <Col style={{ backgroundColor: 'transparent', borderRightWidth: 0.5, borderRightColor: 'gray', justifyContent: 'center' }}>
-                    <Text style={styles.topValue}> {yearOfExperience} </Text>
+                    <Text style={styles.topValue}> {doctorData.experience.year != undefined && doctorData.experience.year != 0 ? doctorData.experience.year + ' yrs' : 'N/A'} </Text>
                     <Text note style={styles.bottomValue}> Experience</Text>
                   </Col>
                   <Col style={{ backgroundColor: 'transparent', justifyContent: 'center', marginLeft: 'auto', marginRight: 'auto' }}>
@@ -301,7 +303,7 @@ class AppointmentDetails extends Component {
                   <View style={{ marginLeft: 'auto', marginRight: 'auto' }}>
                     <Col style={{ width: 300, }}>
                       <Button disabled={true} block style={{ borderRadius: 10, backgroundColor: '#D7BDE2' }}>
-                        <Text style={{ color: 'black', fontSize: 15,fontFamily:'OpenSans',fontWeight:'bold' }}>
+                        <Text style={{ color: 'black', fontSize: 15, fontFamily: 'OpenSans', fontWeight: 'bold' }}>
                           {this.state.appointmentStatus == 'APPROVED' ? 'APPROVED' :
                             data.appointment_status == 'PROPOSED_NEW_TIME' ? 'PROPOSED NEW TIME' :
                               data.appointment_status == 'PENDING_REVIEW' ? 'COMPLETED' :
@@ -317,16 +319,16 @@ class AppointmentDetails extends Component {
                   {data.appointment_status == 'APPROVED' || this.state.appointmentStatus === 'APPROVED' ?
                     <Col style={width = 'auto'}>
                       <Button block danger style={{ margin: 1, marginTop: 10, marginLeft: 1, borderRadius: 30, padding: 15, height: 40, width: "auto" }} onPress={() => this.navigateCancelAppoointment()} testID='cancelAppointment'>
-                        <Text style={{ textAlign: 'center', fontFamily: 'OpenSans',fontSize:15,fontWeight:'bold' }}>CANCEL APPOINTMENT</Text>
+                        <Text style={{ textAlign: 'center', fontFamily: 'OpenSans', fontSize: 15, fontWeight: 'bold' }}>CANCEL APPOINTMENT</Text>
                       </Button>
                     </Col> :
                     data.appointment_status == 'PROPOSED_NEW_TIME' ?
                       <Item style={{ borderBottomWidth: 0, justifyContent: 'center' }}>
                         <Button success style={styles.statusButton} onPress={() => this.updateAppointmentStatus(data, 'APPROVED')} testID='approvedAppointment'>
-                          <Text style={{ textAlign: 'center', fontFamily: 'OpenSans', color: '#000',fontSize:15,fontWeight:'bold' }}>ACCEPT</Text>
+                          <Text style={{ textAlign: 'center', fontFamily: 'OpenSans', color: '#000', fontSize: 15, fontWeight: 'bold' }}>ACCEPT</Text>
                         </Button>
                         <Button danger style={styles.Button2} onPress={() => this.navigateCancelAppoointment()} testID='appointmentCancel'>
-                          <Text style={{ textAlign: 'center', fontFamily: 'OpenSans', color: '#000',fontSize:15,fontWeight:'bold'}}> CANCEL </Text></Button>
+                          <Text style={{ textAlign: 'center', fontFamily: 'OpenSans', color: '#000', fontSize: 15, fontWeight: 'bold' }}> CANCEL </Text></Button>
                       </Item> : null}
                 </Grid>
 
@@ -337,7 +339,7 @@ class AppointmentDetails extends Component {
               <Card style={{ backgroundColor: '#ffffff', borderRadius: 10, padding: 10 }}>
                 <Grid style={{ margin: 5 }}>
                   <Right>
-                    <Text style={{fontSize:15,fontFamily:'OpenSans'}}>
+                    <Text style={{ fontSize: 15, fontFamily: 'OpenSans' }}>
                       {formatDate(data.appointment_starttime, "dddd,MMMM DD-YYYY  hh:mm a")}
                     </Text>
                   </Right>
@@ -541,18 +543,18 @@ const styles = StyleSheet.create({
   topValue: {
     marginLeft: 'auto',
     marginRight: 'auto',
-    fontFamily:'OpenSans',
-    fontSize:15,
-    fontWeight:'bold'
+    fontFamily: 'OpenSans',
+    fontSize: 15,
+    fontWeight: 'bold'
 
   },
   bottomValue:
   {
     marginLeft: 'auto',
     marginRight: 'auto',
-    fontSize:15,
-    fontFamily:'OpenSans',
-  
+    fontSize: 15,
+    fontFamily: 'OpenSans',
+
   },
   reviewButton: {
     marginTop: 12,
@@ -570,7 +572,7 @@ const styles = StyleSheet.create({
     fontFamily: 'OpenSans',
     color: '#000',
     fontSize: 15,
-    
+
 
   },
   subtitlesText: {
@@ -578,7 +580,7 @@ const styles = StyleSheet.create({
     margin: 10,
     color: '#F2889B',
     fontFamily: 'opensans-semibold',
-    fontWeight:'bold'
+    fontWeight: 'bold'
 
   },
   titlesText: {
