d12b892d5b6890bbf93e98113b32e37a220e449a
bala <balankottiyattil@gmail.com>
2019-09-25 15:33:54 +0530

    solved design issues in payment success and previewpage

--- src/modules/screens/PaymentReview/index.js
+++ src/modules/screens/PaymentReview/index.js
@@ -116,7 +116,7 @@ export default class PaymentReview extends Component {
                             <Thumbnail square source={renderDoctorImage(bookSlotDetails)}   style={{ height: 70, width: 70, borderRadius: 10 }} />
                          </Col> 
                          <Col style={{width:'80%',marginTop:10}}>
-                            <Text style={styles.cardItemText}>{bookSlotDetails.prefix || ''} {bookSlotDetails.doctorName} {getDoctorEducation(bookSlotDetails.education)}</Text>
+                            <Text style={styles.cardItemText}>{bookSlotDetails.prefix || ''}{bookSlotDetails.doctorName}{getDoctorEducation(bookSlotDetails.education)}</Text>
                             <Text style={styles. cardItemText2}>{getAllSpecialist(bookSlotDetails.specialist)}</Text>
                           </Col>
                        </Row>
@@ -206,21 +206,20 @@ const styles = StyleSheet.create({
          cardItemText:{
          fontFamily:'OpenSans',
          fontSize:20,
-        height:30,
         fontWeight:'bold',
          color:'#FFF',paddingBottom:-10
          },
          cardItemText2:{
              fontFamily:'OpenSans',
              fontSize:14,
-            height:30,
             fontWeight:'bold',
              color:'#FFF',
-            paddingBottom:-10
+            paddingBottom:-10,
+            width:'90%'
              },
              cardItemText3:{
                  fontFamily:'OpenSans',
-                 fontSize:22,
+                 fontSize:18,
                 height:30,
                 fontWeight:'bold',
                  color:'#FFF',paddingBottom:-10
