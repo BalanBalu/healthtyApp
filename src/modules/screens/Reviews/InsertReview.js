d7d06eddb65d37d06c553af5b407c94d59a09d98
bala <balankottiyattil@gmail.com>
2020-01-08 12:30:05 +0530

    alignment issues fixed

--- src/modules/screens/Reviews/InsertReview.js
+++ src/modules/screens/Reviews/InsertReview.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 
-import { StyleSheet, Image, TextInput, Dimensions, AsyncStorage, Modal ,TouchableOpacity,TextInput} from 'react-native';
+import { StyleSheet, Image, TextInput, Dimensions, AsyncStorage, Modal, TouchableOpacity, TextInput } from 'react-native';
 import StarRating from 'react-native-star-rating';
 import {
-  Container, Header, Title, Left, Right, Body, Button, Card, Toast, CardItem, Row, Grid, View,Col,
+  Container, Header, Title, Left, Right, Body, Button, Card, Toast, CardItem, Row, Grid, View, Col,
   Text, Thumbnail, Content, CheckBox, Item, Input
 } from 'native-base';
 import { Checkbox } from 'react-native-paper';
@@ -96,10 +96,10 @@ class InsertReview extends Component {
         console.log(JSON.stringify(result))
 
         if (result.success) {
-        
+
           const { navigation } = this.props;
           const { routeName, key } = navigation.getParam('prevState');
-          if(routeName === 'AppointmentInfo') {
+          if (routeName === 'AppointmentInfo') {
             navigation.navigate({ routeName, key, params: { hasReloadReview: true } });
           } else {
             navigation.pop()
@@ -135,143 +135,141 @@ class InsertReview extends Component {
   }
 
   render() {
-    const { data,checked } = this.state;
+    const { data, checked } = this.state;
     return (
       < Container style={styles.container} >
         <Content>
 
-        <View>
-																<Modal
-																animationType="slide"
-																transparent={true}
-																visible={this.state.modalVisible}
-																>
-																  <View style={{
-                                                                              flex: 1,
-                                                                              flexDirection: 'column',
-                                                                              justifyContent: 'flex-end',
-																			  alignItems: 'center',
-																			  height: '50%',
-																			  transparent:'rgba(0,0,0,0.7)',
-																			  overlayColor: 'rgba(0, 0, 0, 0.7)',
-																			  backgroundColor: 'rgba(0,0,0,0.2)'
+          <View>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              visible={this.state.modalVisible}
+            >
+              <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'flex-end',
+                alignItems: 'center',
+                height: '50%',
+                transparent: 'rgba(0,0,0,0.7)',
+                overlayColor: 'rgba(0, 0, 0, 0.7)',
+                backgroundColor: 'rgba(0,0,0,0.2)'
 
-                                                                          }}>
-                                                                              <View style={{
-                                                                                  width: '95%',
-                                                                                  height: '55%', backgroundColor: '#fff',
-                                                                               
-            
-																				  borderRadius: 5,
-																				 
-                                                                              }}>
-																			 <Content>
-																				  <Grid>
-						                                               		<Row style={{backgroundColor:'#7F49C3',paddingTop:10,paddingBottom:10,paddingLeft:10,paddingRight:10,borderTopLeftRadius:5,borderTopRightRadius:5}}>
-																				   <Left>
-																					   <Text style={{color:'#fff',fontSize:16}}>Mukesh Kumar</Text>
-																				   </Left>
-																				   <Right>
-																					   <Text  style={{color:'#fff',fontSize:12}}>January 2,2020 - 05.30 PM</Text>
-																				   </Right>
-																			   </Row>
-                                                                              <View>
-																				
-																			   <Row  style={{marginTop:20}}>
-																				   <Left style={{marginLeft:20}}>
-																					   <Text style={{fontFamily:'OpenSans',fontSize:16}}>Cleanliness</Text>
-																				   </Left>
-																				   <Right style={{marginRight:20}}>
-																				   <StarRating fullStarColor='#FF9500' starSize={15} containerStyle={{ width: 110, marginLeft: 50 }}
-                                                                                       disabled={false}
-                                                                                       maxStars={5}
-                                                                                       rating={this.state.cleanness_rating}
-                                                                                       selectedStar={(rating) => this.CleanlinessStarRating(rating)}
-
-                                                                                     />
-																				   </Right>
-																			   </Row>
-																			   <Row style={{marginTop:20}}>
-																				   <Left style={{marginLeft:20}}>
-																					   <Text  style={{fontFamily:'OpenSans',fontSize:16}}>Staff</Text>
-																				   </Left>
-																				   <Right style={{marginRight:20}}>
-																				   <StarRating fullStarColor='#FF9500' starSize={15} containerStyle={{ width: 110, marginLeft: 97 }}
-                                                                                  disabled={false}
-                                                                                  maxStars={5}
-                                                                                  rating={this.state.staff_rating}
-                                                                                  selectedStar={(rating) => this.staffStarRating(rating)}
-
-                                                                                />
-																				   </Right>
-																			   </Row>
-																			   <Row style={{marginTop:20}}>
-																				   <Left style={{marginLeft:20}}>
-																					   <Text   style={{fontFamily:'OpenSans',fontSize:16}}>Wait Time</Text>
-																				   </Left>
-																				   <Right style={{marginRight:20}}>
-																				   <StarRating fullStarColor='#FF9500' starSize={15} containerStyle={{ width: 110, marginLeft: 60 }}
-                                                                                        disabled={false}
-                                                                                        maxStars={5}
-                                                                                        rating={this.state.wait_time_rating}
-                                                                                        selectedStar={(rating) => this.waittimeStarRating(rating)}
-
-                                                                                        />
-																				   </Right>
-																			   </Row>
-																			   <Row style={{marginTop:20}}>
-																				   <Col style={{marginLeft:10,flexDirection:'row',width:'49%'}}>
-																				   <Checkbox  color="#3C98EC" size={5}
-                                                                                              status={checked ? 'checked' : 'unchecked'}
-                                                                                              onPress={() => { this.setState({ checked: !checked }); }}
-																							  style={{height:5,width:5}} />
-								                                                   <Text style={{color:'#3C98EC',marginTop:10,fontSize:12}}>Anonymous</Text>
-																				   </Col>
-																				   <Col style={{flexDirection:'row',width:'51%'}}>
-																					   
-																				   <Checkbox  color="#3C98EC" size={5}
-                                                                                              status={checked ? 'checked' : 'unchecked'}
-                                                                                              onPress={() => { this.setState({ checked: !checked }); }}
-                                                                                               />
-																							
-								                                                   <Text style={{color:'#3C98EC',fontSize:12,marginTop:10}}>Recommend this Doctor</Text>
-																				   </Col>
-																			   </Row>
-																			   <View style={{marginLeft:20,marginTop:10,marginRight:20}}>
-																			   <TextInput
-                                                                                     style={{ height: 80, borderWidth: 1, marginTop: 10, width: "100%",borderRadius:5,fontSize:14}}
-																					 returnKeyType={'next'}
-																					 placeholder = "Write Your Reviews"
-                                                                                     multiline={true}
-                                                                                     keyboardType={'default'}
-                                                                                     textAlignVertical={'top'}
-                                                                                     onChangeText={(comments) => {
-                                                                                       this.setState({ comments })}
-                                                                                     }/>
-																					 </View>
-																					 </View>
-																					 <Row style={{marginLeft:20,marginTop:10,marginRight:20}}>
-																					
-																						<Col style={{width:'48%'}}>
-																						</Col>	
-																						<Col style={{width:'52%',flexDirection:'row'}}>
-																						<TouchableOpacity style={{backgroundColor:'#959595',paddingLeft:20,paddingRight:20,paddingTop:1,paddingBottom:3,borderRadius:2}}><Text uppercase={true} style={{color:'#FFF',fontSize:12,}} >Cancel</Text></TouchableOpacity>
-																							 <TouchableOpacity style={{backgroundColor:'#349631',paddingLeft:20,paddingRight:20,paddingTop:1,paddingBottom:3,borderRadius:2,marginLeft:10}}><Text uppercase={true} style={{color:'#FFF',fontSize:12}}>Submit</Text></TouchableOpacity>
-																						</Col>	
-																						
-																						
-																					 </Row>
-																			 </Grid>
-																			 </Content>
-													  
-																
-																  </View>
-																</View>
-															  </Modal>
-															  </View>
+              }}>
+                <View style={{
+                  width: '95%',
+                  height: '55%', backgroundColor: '#fff',
 
 
+                  borderRadius: 5,
 
+                }}>
+                  <Content>
+                    <Grid>
+                      <Row style={{ backgroundColor: '#7F49C3', paddingTop: 10, paddingBottom: 10, paddingLeft: 10, paddingRight: 10, borderTopLeftRadius: 5, borderTopRightRadius: 5 }}>
+                        <Left>
+                          <Text style={{ color: '#fff', fontSize: 16 }}>Mukesh Kumar</Text>
+                        </Left>
+                        <Right>
+                          <Text style={{ color: '#fff', fontSize: 12 }}>January 2,2020 - 05.30 PM</Text>
+                        </Right>
+                      </Row>
+                      <View>
+
+                        <Row style={{ marginTop: 20 }}>
+                          <Left style={{ marginLeft: 20 }}>
+                            <Text style={{ fontFamily: 'OpenSans', fontSize: 16 }}>Cleanliness</Text>
+                          </Left>
+                          <Right style={{ marginRight: 20 }}>
+                            <StarRating fullStarColor='#FF9500' starSize={15} containerStyle={{ width: 110, marginLeft: 50 }}
+                              disabled={false}
+                              maxStars={5}
+                              rating={this.state.cleanness_rating}
+                              selectedStar={(rating) => this.CleanlinessStarRating(rating)}
+
+                            />
+                          </Right>
+                        </Row>
+                        <Row style={{ marginTop: 20 }}>
+                          <Left style={{ marginLeft: 20 }}>
+                            <Text style={{ fontFamily: 'OpenSans', fontSize: 16 }}>Staff</Text>
+                          </Left>
+                          <Right style={{ marginRight: 20 }}>
+                            <StarRating fullStarColor='#FF9500' starSize={15} containerStyle={{ width: 110, marginLeft: 97 }}
+                              disabled={false}
+                              maxStars={5}
+                              rating={this.state.staff_rating}
+                              selectedStar={(rating) => this.staffStarRating(rating)}
+
+                            />
+                          </Right>
+                        </Row>
+                        <Row style={{ marginTop: 20 }}>
+                          <Left style={{ marginLeft: 20 }}>
+                            <Text style={{ fontFamily: 'OpenSans', fontSize: 16 }}>Wait Time</Text>
+                          </Left>
+                          <Right style={{ marginRight: 20 }}>
+                            <StarRating fullStarColor='#FF9500' starSize={15} containerStyle={{ width: 110, marginLeft: 60 }}
+                              disabled={false}
+                              maxStars={5}
+                              rating={this.state.wait_time_rating}
+                              selectedStar={(rating) => this.waittimeStarRating(rating)}
+
+                            />
+                          </Right>
+                        </Row>
+                        <Row style={{ marginTop: 20 }}>
+                          <Col style={{ marginLeft: 10, flexDirection: 'row', width: '49%' }}>
+                            <Checkbox color="#3C98EC" size={5}
+                              status={checked ? 'checked' : 'unchecked'}
+                              onPress={() => { this.setState({ checked: !checked }); }}
+                              style={{ height: 5, width: 5 }} />
+                            <Text style={{ color: '#3C98EC', marginTop: 10, fontSize: 12 }}>Anonymous</Text>
+                          </Col>
+                          <Col style={{ flexDirection: 'row', width: '51%' }}>
+
+                            <Checkbox color="#3C98EC" size={5}
+                              status={checked ? 'checked' : 'unchecked'}
+                              onPress={() => { this.setState({ checked: !checked }); }}
+                            />
+
+                            <Text style={{ color: '#3C98EC', fontSize: 12, marginTop: 10 }}>Recommend this Doctor</Text>
+                          </Col>
+                        </Row>
+                        <View style={{ marginLeft: 20, marginTop: 10, marginRight: 20 }}>
+                          <TextInput
+                            style={{ height: 80, borderWidth: 1, marginTop: 10, width: "100%", borderRadius: 5, fontSize: 14 }}
+                            returnKeyType={'next'}
+                            placeholder="Write Your Reviews"
+                            multiline={true}
+                            keyboardType={'default'}
+                            textAlignVertical={'top'}
+                            onChangeText={(comments) => {
+                              this.setState({ comments })
+                            }
+                            } />
+                        </View>
+                      </View>
+                      <Row style={{ marginLeft: 20, marginTop: 10, marginRight: 20 }}>
+
+                        <Col style={{ width: '48%' }}>
+                        </Col>
+                        <Col style={{ width: '52%', flexDirection: 'row' }}>
+                          <TouchableOpacity style={{ backgroundColor: '#959595', paddingLeft: 20, paddingRight: 20, paddingTop: 1, paddingBottom: 3, borderRadius: 2 }}><Text uppercase={true} style={{ color: '#FFF', fontSize: 12, }} >Cancel</Text></TouchableOpacity>
+                          <TouchableOpacity style={{ backgroundColor: '#349631', paddingLeft: 20, paddingRight: 20, paddingTop: 1, paddingBottom: 3, borderRadius: 2, marginLeft: 10 }}><Text uppercase={true} style={{ color: '#FFF', fontSize: 12 }}>Submit</Text></TouchableOpacity>
+                        </Col>
+
+
+                      </Row>
+                    </Grid>
+                  </Content>
+
+
+                </View>
+              </View>
+            </Modal>
+          </View>
 
 
 
@@ -289,7 +287,10 @@ class InsertReview extends Component {
 
 
 
-{/* 
+
+
+
+          {/* 
           <Card style={{ borderRadius: 5, padding: 5, }}>
             <Card>
               <CardItem style={styles.text}>
@@ -387,10 +388,10 @@ class InsertReview extends Component {
                       borderRadius: 5
                     }}> */}
 
-                      {/* <Item regular rounded style={{ borderColor: '#000', borderWidth: 2, marginTop: 20 }}> */}
-                   
-                      {/* </Item> */}
-                      {/* <Row style={{ marginTop: 10,justifyContent:'center' }}>
+          {/* <Item regular rounded style={{ borderColor: '#000', borderWidth: 2, marginTop: 20 }}> */}
+
+          {/* </Item> */}
+          {/* <Row style={{ marginTop: 10,justifyContent:'center' }}>
                         <Col style={{justifyContent:'center',width:'80%',marginTop:-30}}>
                         <Text style={{ fontFamily: 'OpenSans',textAlign:'center', }}> Kindly give rating for your Review! </Text>
 
