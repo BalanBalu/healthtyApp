d9fe3bf45dcadc51ad49eaac661eb3b22116d0fd
Surya <arul@alldirectionsource.com>
2020-04-22 23:50:37 +0530

    height and weight implementation complete

---
+++ src/modules/screens/userprofile/Updateheightweight.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+import { Container, Content, Text, Button, H3, Item, List, View, CheckBox, Left, Right, Thumbnail, Body, Icon, Card, Input, Toast, Row } from 'native-base';
+import { AsyncStorage, ScrollView } from 'react-native';
+import { userFiledsUpdate } from '../../providers/auth/auth.actions';
+import { connect } from 'react-redux'
+import styles from './style.js'
+import Spinner from '../../../components/Spinner';
+import { Col } from 'react-native-easy-grid';
+import { validatePassword } from '../../common'
+
+
+
+class Updateheightweight extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            setWeight: '',
+            setHeight: '',
+            isLoading: false,
+            errorMsg: '',
+        }
+    }
+
+    componentDidMount() {
+        this.UpdateHeightAndWeight();
+    }
+
+    UpdateHeightAndWeight= async () => {    
+        try {
+            let userId = await AsyncStorage.getItem('userId');
+            let data = {
+              
+                    height: this.state.setHeight,
+                    weight: this.state.setWeight,
+                
+            };
+
+            this.setState({  isLoading: true });
+
+            let response = await userFiledsUpdate(userId, data);
+            if (response.success) {
+                Toast.show({
+                    text: response.message,
+                    type: "success",
+                    duration: 3000,
+                })
+               
+            } 
+            this.setState({ isLoading: false });
+
+        }
+        catch (e) {
+            console.log(e);
+        }
+        finally {
+            this.setState({ isLoading: false });
+        } 
+}
+   
+    render() {
+
+        return (
+            <Container style={styles.container}>
+
+                <Content contentContainerStyle={styles.bodyContent}>
+                    <ScrollView>
+                        <Spinner color='blue'
+                            visible={this.state.isLoading}
+                        />
+
+
+                        <Text style={styles.headerText}>Update</Text>
+                        <View style={styles.cardEmail}>
+                        <Icon name="ios-body" style={styles.centeredIcons}></Icon>
+                       
+                            <Item style={{ borderBottomWidth: 0 }}>
+                            
+                            <Text style={{ fontSize: 15, fontFamily: 'OpenSans', marginTop: 5, marginRight: 10 }} >Height : </Text>
+
+                                <Col style={styles.transparentLabel1}>
+                                    
+                                    <Row>
+                                        <Input placeholder="Height in cm"
+                                            style={{ fontSize: 13, fontFamily: 'OpenSans', marginTop: -5 }}
+                                            value={this.state.setHeight}
+                                             onChangeText={(setHeight) => this.setState({setHeight})}
+                                            /> 
+                                       <Text style={{ fontSize: 13, fontFamily: 'OpenSans', marginTop:10, marginRight:50 }}>cm</Text>
+                                    </Row>
+                                </Col>
+
+
+                            </Item>
+                            <Item style={{ borderBottomWidth: 0 }}>
+                            <Text style={{ fontSize: 15, fontFamily: 'OpenSans', marginTop: 5, marginRight: 10 }} >Weight : </Text>
+                                <Col style={styles.transparentLabel1}>
+                                    <Row>
+                                        <Input placeholder="Weight in kg"
+                                             style={{ fontSize: 13, fontFamily: 'OpenSans', marginTop: -5 }}
+                                             value={this.state.setWeight}
+                                             onChangeText={(setWeight) => this.setState({setWeight})}
+                                             />
+                                        <Text style={{ fontSize: 13, fontFamily: 'OpenSans', marginTop:10, marginRight:50 }}>kg</Text>
+                                    </Row>
+                                </Col>
+
+                            </Item>
+                            {this.state.errorMsg ? <Text style={{ paddingLeft: 20, fontSize: 13, fontFamily: 'OpenSans', color: 'red' }}>{this.state.errorMsg}</Text> : null}
+
+                            <Item style={{ borderBottomWidth: 0, marginTop: 10 }}>
+                                <Right>
+                                    <Button success style={styles.button2} onPress={() => this.UpdateHeightAndWeight()} testID='clickUpdateHeightAndWidth'>
+                                        <Text uppercase={false} note style={styles.buttonText}>Update</Text>
+                                    </Button>
+                                </Right>
+                            </Item>
+
+
+                        </View>
+
+                    </ScrollView>
+                </Content>
+
+            </Container>
+
+        )
+    }
+
+}
+
+function profileState(state) {
+
+    return {
+        user: state.user
+    }
+}
+export default connect(profileState)(Updateheightweight)
+
+
