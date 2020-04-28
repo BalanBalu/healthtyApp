27b6bf625cd6f491d932886bf6b233e6a6a8c2d4
Shobana <shobanarathnakumar@gmail.com>
2020-04-28 21:29:32 +0530

    Lab Category search Patient

--- src/modules/screens/LabTest/categories/index.js
+++ src/modules/screens/LabTest/categories/index.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import { Container, Content, Text, Title, Header, Button, H3, Item, List, ListItem, Card, Left, Right, Thumbnail, Body, Icon, locations, Input } from 'native-base';
 import LinearGradient from 'react-native-linear-gradient';
+import { connect } from 'react-redux'
 import { Col, Row, Grid } from 'react-native-easy-grid';
+import { toDataUrl } from '../../../../setup/helpers';
 import { StyleSheet, Image, TouchableOpacity, View, FlatList } from 'react-native';
-import { MAX_DISTANCE_TO_COVER } from '../../../../setup/config';
+import { getLabTestCateries } from '../../../providers/lab/lab.action'
+import { MAX_DISTANCE_TO_COVER } from '../../../../setup/config'
 
 
 class LabCategories extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      labData: [],
       data: [],
-      categoriesMain: []
     }
   }
   async componentDidMount() {
+    this.getLabCategories();
   }
+
+
   onPressCatItem = async (type, value) => {
     const inputDataBySearch = [
       // {
@@ -35,6 +41,61 @@ class LabCategories extends Component {
     console.log('cate inputDataBySearch=====>', inputDataBySearch)
     this.props.navigation.navigate('LabSearchList', { inputDataFromLabCat: inputDataBySearch })
   }
+
+
+  getLabCategories = async () => {
+    try {
+      const { bookappointment: { locationCordinates } } = this.props;
+      console.log("locationCordinates", locationCordinates)
+      // locationData = {
+      //   "coordinates": locationCordinates,
+      //   "maxDistance": MAX_DISTANCE_TO_COVER
+      // }
+     let locationData = {
+        "coordinates": [
+          13.0423185,
+          80.196269
+        ]
+      }
+      let result = await getLabTestCateries(JSON.stringify(locationData));
+      console.log("result", result)
+      if (result.success) {
+        this.setState({ data: result.data, labData: result.data })
+        for (let i = 0; i < result.data.length; i++) {
+          const value = result.data[i];
+          const imageURL = value.lab_test_category_info.category_image_url + value.lab_test_category_info.category_image_name;
+          console.log("imageURL", imageURL)
+
+          const finalImageUrl = await toDataUrl(imageURL)
+          result.data[i].baseImageURL = finalImageUrl;
+          this.setState({ labData: result.data, data:result.data })
+          console.log("labData", this.state.labData)
+
+        }
+      }
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }
+
+
+  filterCategories(searchValue) {
+    console.log(this.state.data);
+    const { labData } = this.state;
+    if (!searchValue) {
+      this.setState({ searchValue, data: labData });
+    } else {
+      const filteredCategories = labData.filter(ele =>
+        ele.lab_test_category_info.category_name.toLowerCase().search(searchValue.toLowerCase()) !== -1
+      );
+      this.setState({ searchValue, data: filteredCategories })
+      console.log("filteredCategories",this.state.filteredCategories);
+
+    }
+  }
+
+
   renderStickeyHeader() {
     return (
       <View style={{ width: '100%' }} >
@@ -63,40 +124,32 @@ class LabCategories extends Component {
     )
   }
   render() {
-    const data = [
-
-      // { checkup: 'arthritis', initalprice: 500, finalprice: 400 },
-      // { checkup: 'Allergy Profile', initalprice: 200, finalprice: 100 }
-      { checkup: 'category1', initalprice: 500, finalprice: 400 },
-      { checkup: 'category2', initalprice: 200, finalprice: 100 }, { checkup: 'full body checkup', initalprice: 2500, finalprice: 1500 },
-      { checkup: 'Diabetes Test', initalprice: 2500, finalprice: 1500 },
-      { checkup: 'Fever Test', initalprice: 1500, finalprice: 1000 },
-      { checkup: 'Healthy men', initalprice: 250, finalprice: 150 }]
     return (
       <Container style={styles.container}>
         <Content style={styles.bodyContent}>
 
           <View style={{ marginBottom: 10 }}>
             <FlatList horizontal={false} numColumns={3}
-              data={data}
-              // extraData={this.state}
+              data={this.state.data}
+             extraData={this.state}
               ListHeaderComponent={this.renderStickeyHeader()}
               renderItem={({ item, index }) =>
                 <Col style={styles.mainCol}>
                   <TouchableOpacity
-                    onPress={() => this.onPressCatItem('category_name', item.checkup)}
+                    onPress={() => this.onPressCatItem('category_name', item.lab_test_category_info.category_name)}
                     style={{ justifyContent: 'center', alignItems: 'center', width: '100%', paddingTop: 5, paddingBottom: 5 }}>
+
                     <Image
-                      source={require('../../../../../assets/images/labCategories/Diabetes.png')}
+                      source={{ uri: item.baseImageURL }}
                       style={{
                         width: 60, height: 60, alignItems: 'center'
                       }}
                     />
-                    <Text style={styles.mainText}>{item.checkup}</Text>
+                    <Text style={styles.mainText}>{item.lab_test_category_info.category_name}</Text>
                     <Text style={styles.subText}>package starts from</Text>
                     <Row>
-                      <Text style={styles.rsText}>₹ {item.initalprice}</Text>
-                      <Text style={styles.finalRs}>₹ {item.finalprice}</Text>
+                      <Text style={styles.rsText}>₹ {item.minPriceWithoutOffer}</Text>
+                      <Text style={styles.finalRs}>₹ {Math.round(item.minPriceWithOffer)}</Text>
                     </Row>
                   </TouchableOpacity>
                 </Col>
@@ -111,8 +164,13 @@ class LabCategories extends Component {
   }
 
 }
+function labCategoriesState(state) {
 
-export default LabCategories
+  return {
+    bookappointment: state.bookappointment,
+  }
+}
+export default connect(labCategoriesState)(LabCategories)
 
 
 const styles = StyleSheet.create({
