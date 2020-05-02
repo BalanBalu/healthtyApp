337f4b62d7a5ad5a77adaeb63e881e6490c5db0b
Sathish Krishnan <sathishkrish20@gmail.com>
2020-05-02 22:33:28 +0000

    config.js edited online with Bitbucket

--- src/setup/config.js
+++ src/setup/config.js
@@ -6,7 +6,7 @@ export const IS_IOS = Platform.OS === 'ios';
 
 export const API_URL = 'https://medflic-qa-api.aopks.com/api/';
 export const CATEGORY_BASE_URL = API_URL.substring(0, API_URL.length - 4) + 'images/category/';
-export const CHAT_API_URL = 'https://chat-api.aopks.com';
+export const CHAT_API_URL = 'https://chat-qa-api.aopks.com';
 
 export const MAP_BOX_TOKEN = 'sk.eyJ1IjoidmFpcmFpc2F0aGlzaCIsImEiOiJjand4NWV2djQwZGFkNDNtejhkYXVwbW0zIn0.SxLkBv_NwpDKUIl-e499rg';
 export const MAP_BOX_PUBLIC_TOKEN = 'pk.eyJ1IjoidmFpcmFpc2F0aGlzaCIsImEiOiJjanZhMjBrZHMwdW16NGVtamFhNnE4aDIwIn0.s629-J2w7AeQITCLhdl0pw'
