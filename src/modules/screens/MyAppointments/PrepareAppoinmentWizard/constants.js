216801cae0f5a4b07de265634f73241c705bcce3
Bala <balankottiyattil@gmail.com>
2020-05-06 22:44:49 +0530

    updated the changes

--- src/modules/screens/MyAppointments/PrepareAppoinmentWizard/constants.js
+++ src/modules/screens/MyAppointments/PrepareAppoinmentWizard/constants.js
@@ -1,25 +1,29 @@
 export const disease = [
-    { disease: 'AIDS/HIV', id: 0 }, { disease: 'Alcoholic', id: 1 },
-    { disease: 'Allergies', id: 2 }, { disease: 'Anemia', id: 3 },
-    { disease: 'Anxiety', id: 4 }, { disease: 'Arthritis', id: 5 },
-    { disease: 'Asthma', id: 6 }, { disease: 'Back Problems/Pain', id: 7 },
-    { disease: 'Bleeding Disorder', id: 8 }, { disease: 'Blood Disease', id: 9 },
-    { disease: 'Bone/Joint Disease', id: 10 }, { disease: 'Cancer', id: 11 },
-    { disease: 'Depression', id: 12 }, { disease: 'Diabetes', id: 13 },
-    { disease: 'Ear Problem', id: 14 }, { disease: 'Eating Disorder', id: 15 },
-    { disease: 'Alcoholic', id: 16 }
+    { disease: 'AIDS/HIV', }, { disease: 'Alcoholic',  },
+    { disease: 'Allergies', }, { disease: 'Anemia', },
+    { disease: 'Anxiety',}, { disease: 'Arthritis',  },
+    { disease: 'Asthma',}, { disease: 'Back Problems/Pain',  },
+    { disease: 'Bleeding Disorder',  }, { disease: 'Blood Disease',  },
+    { disease: 'Bone/Joint Disease',  }, { disease: 'Cancer',},
+    { disease: 'Depression',  }, { disease: 'Diabetes',  },
+    { disease: 'Ear Problem', }, { disease: 'Eating Disorder',  },
+  
 ]
 export const bloodGroupList = ['Select number', 'A+', 'O+', 'B+', 'AB+', 'A-', 'O-', 'B-', 'AB-'];
 export const allergic = [
-    { disease: 'Adhesive Tape', id: 0 },
-    { disease: 'Antibiotics', id: 1 },
-    { disease: 'Aspirin', id: 2 },
-    { disease: 'Barbituates', id: 3 },
-    { disease: 'Codeine', id: 4 },
-    { disease: 'Lodine', id: 5 },
-    { disease: 'Latex', id: 6 },
-    { disease: 'Local Anesthetics', id: 7 },
-    { disease: 'Sulfa', id: 8 },
-    { disease: 'Non of the above', id: 9 },
-    { disease: 'Not sure', id: 10 }]
+    { disease: 'Adhesive Tape',  },
+    { disease: 'Antibiotics',  },
+    { disease: 'Aspirin',  },
+    { disease: 'Barbituates',  },
+    { disease: 'Codeine', },
+    { disease: 'Lodine', },
+    { disease: 'Latex',  },
+    { disease: 'Local Anesthetics',  },
+    { disease: 'Sulfa',  },
+    { disease: 'Non of the above',  },
+    { disease: 'Not sure',}]
 export const numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+export const hospitalizedFor = [
+    { condition: 'Blood Transfusion' }, 
+    { condition: 'None of the above' }, 
+    { condition: 'Not sure' }]
