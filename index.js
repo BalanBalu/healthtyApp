fb6ede02cf2e88bb6e4cb0eef3064e93ceec6b50
Sathish Krishnan <sathiskrish20@gmail.com>
2020-05-10 19:45:51 +0530

    updated-the-ios-plugin-updates

--- index.js
+++ index.js
@@ -3,8 +3,17 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import {AppRegistry} from 'react-native';
+import {AppRegistry, NativeModules} from 'react-native';
 import App from './src/setup';
 import {name as appName} from './app.json';
-
+import messaging from '@react-native-firebase/messaging';
+import rootNavigation from './src/setup/rootNavigation';
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+    if(remoteMessage.data && remoteMessage.data.videoNotification == '1') {
+       // rootNavigation.navigate('Home');
+    }
+   // NativeModules.ActivityStarter.navigateToExample();
+    Promise.resolve();
+});
 AppRegistry.registerComponent(appName, () => App);
