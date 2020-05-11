969b68cb425011d6187f672197719fd130242273
Bala <balankottiyattil@gmail.com>
2020-05-11 23:02:12 +0530

    aligned the file properly

--- src/modules/screens/Pharmacy/MedicineInfo/medInsertReview.js
+++ src/modules/screens/Pharmacy/MedicineInfo/medInsertReview.js
@@ -8,7 +8,7 @@ import { StyleSheet, Image, TouchableOpacity, AsyncStorage, FlatList, TouchableH
 import { TextInput } from 'react-native-gesture-handler';
 import { formatDate } from '../../../../setup/helpers';
 import StarRating from 'react-native-star-rating';
-import { InsertMedicineReviews,InsertOrderReviews } from '../../../providers/pharmacy/pharmacy.action'
+import { InsertMedicineReviews, InsertOrderReviews } from '../../../providers/pharmacy/pharmacy.action'
 import { hasLoggedIn } from "../../../providers/auth/auth.actions";
 
 export class MedInsertReview extends Component {
@@ -167,13 +167,13 @@ export class MedInsertReview extends Component {
                   <Row style={{ marginTop: 20, marginLeft: 14, marginRight: 20 }}>
 
                     <Col style={{ flexDirection: 'row', width: '45%', alignItems: "center", justifyContent: 'flex-start' }}>
-                          <CheckBox  
-                            status={this.state.is_anonymous ? true : false}
-                            style={{borderRadius:5}}
-                            checked={this.state.is_anonymous}
-                            onPress={() => { this.setState({ is_anonymous: !is_anonymous }); }}
-                                               />
-                      <Text style={{ color: '#3C98EC', fontSize: 12,marginLeft:20 }}>Anonymous</Text>
+                      <CheckBox
+                        status={this.state.is_anonymous ? true : false}
+                        style={{ borderRadius: 5 }}
+                        checked={this.state.is_anonymous}
+                        onPress={() => { this.setState({ is_anonymous: !is_anonymous }); }}
+                      />
+                      <Text style={{ color: '#3C98EC', fontSize: 12, marginLeft: 20 }}>Anonymous</Text>
                     </Col>
                   </Row>
                   <Row style={{ marginLeft: 20, marginTop: 10, marginRight: 20, marginBottom: 20 }}>
@@ -230,13 +230,13 @@ export class OrderInsertReview extends Component {
         }
 
         this.setState({ errorMsg: '' });
-       
+
         console.log("data", userId)
-          reviewInsertData.is_review_added = true,
+        reviewInsertData.is_review_added = true,
           reviewInsertData.is_anonymous = is_anonymous,
           reviewInsertData.overall_rating = rating,
           reviewInsertData.comments = comments
-      } 
+      }
       else {
         reviewInsertData.is_review_added = false
       }
@@ -266,27 +266,27 @@ export class OrderInsertReview extends Component {
         });
       }
 
-    //  else {
-    //   this.props.popupVisible({
-    //     visible: false,
-    //   });
+      //  else {
+      //   this.props.popupVisible({
+      //     visible: false,
+      //   });
 
-    // }
+      // }
+    }
+    catch (e) {
+      console.log(e)
+    }
+
   }
-  catch(e) {
-    console.log(e)
-  }
-
-}
 
 
 
-render() {
-  const { errorMsg, is_anonymous } = this.state;
-  return (
-    <Container style={{ flex: 1 }}>
-      <Content style={{ backgroundColor: '#EAE6E6', padding: 10, flex: 1 }}>
-        {/* <View>
+  render() {
+    const { errorMsg, is_anonymous } = this.state;
+    return (
+      <Container style={{ flex: 1 }}>
+        <Content style={{ backgroundColor: '#EAE6E6', padding: 10, flex: 1 }}>
+          {/* <View>
               <Row>
                 <TouchableOpacity style={{ borderColor: '#8dc63f', borderWidth: 1, marginLeft: 1, borderRadius: 2.5, height: 25, width: 65, backgroundColor: '#8dc63f' }}
                   onPress={() => {
@@ -298,65 +298,90 @@ render() {
                 </TouchableOpacity>
               </Row>
             </View> */}
-        <View style={{ height: 200, position: 'absolute', bottom: 0 }}>
-          <Modal
-            animationType="slide"
-            transparent={true}
-            backgroundColor='rgba(0,0,0,0.7)'
-            containerStyle={{ justifyContent: 'flex-end', }}
-            visible={this.state.modalVisible}
-            animationType={'slide'}
-          >
-            <Grid style={{
-              backgroundColor: '#fff',
-              position: 'absolute',
-              bottom: 0,
-              marginLeft: 10, marginRight: 10, borderRadius: 5, borderWidth: 1, borderColor: 'grey'
-            }}>
+          <View style={{ height: 200, position: 'absolute', bottom: 0 }}>
+            <Modal
+              animationType="slide"
+              transparent={true}
+              backgroundColor='rgba(0,0,0,0.7)'
+              containerStyle={{ justifyContent: 'flex-end', }}
+              visible={this.state.modalVisible}
+              animationType={'slide'}
+            >
+              <Grid style={{
+                backgroundColor: '#fff',
+                position: 'absolute',
+                bottom: 0,
+                marginLeft: 10, marginRight: 10, borderRadius: 5, borderWidth: 1, borderColor: 'grey'
+              }}>
 
-              <Row style={{ backgroundColor: '#7F49C3', paddingTop: 10, paddingBottom: 10, paddingLeft: 10, paddingRight: 10, borderTopLeftRadius: 5, borderTopRightRadius: 5 }}>
-                <Left>
-                  <Text style={{ color: '#fff', fontSize: 16 }}>{this.state.data.medicine_name}</Text>
-                </Left>
-                <Right>
-                  <Text style={{ color: '#fff', fontSize: 12 }}>{formatDate(new Date(), 'MMMM DD, YYYY - hh.mm a')}</Text>
-                </Right>
-              </Row>
+                <Row style={{ backgroundColor: '#7F49C3', paddingTop: 10, paddingBottom: 10, paddingLeft: 10, paddingRight: 10, borderTopLeftRadius: 5, borderTopRightRadius: 5 }}>
+                  <Left>
+                    <Text style={{ color: '#fff', fontSize: 16 }}>{this.state.data.medicine_name}</Text>
+                  </Left>
+                  <Right>
+                    <Text style={{ color: '#fff', fontSize: 12 }}>{formatDate(new Date(), 'MMMM DD, YYYY - hh.mm a')}</Text>
+                  </Right>
+                </Row>
 
-              <View>
                 <View>
-                  <View style={{ marginTop: 20, justifyContent: 'center', alignItems: 'center' }}>
-                    <StarRating fullStarColor='#FF9500' starSize={30} starStyle={{ marginLeft: 5 }} containerStyle={{}}
-                      disabled={false}
-                      maxStars={5}
-                      rating={this.state.rating}
-                      selectedStar={(rating) => this.setState({ rating })}
-                    />
-                  </View>
-                  <View style={{ marginLeft: 20, marginTop: 10, marginRight: 20 }}>
-                    <TextInput
-                      style={{ height: 80, borderWidth: 0.3, marginTop: 10, width: "100%", borderRadius: 5, fontSize: 14 }}
-                      returnKeyType={'next'}
-                      placeholder="Write Your Reviews"
-                      multiline={true}
-                      keyboardType={'default'}
-                      textAlignVertical={'top'}
-                      onChangeText={(comments) => {
-                        this.setState({ comments })
-                      }
-                      } />
+                  <View>
+                    <View style={{ marginTop: 20, justifyContent: 'center', alignItems: 'center' }}>
+                      <StarRating fullStarColor='#FF9500' starSize={30} starStyle={{ marginLeft: 5 }} containerStyle={{}}
+                        disabled={false}
+                        maxStars={5}
+                        rating={this.state.rating}
+                        selectedStar={(rating) => this.setState({ rating })}
+                      />
+                    </View>
+                    <View style={{ marginLeft: 20, marginTop: 10, marginRight: 20 }}>
+                      <TextInput
+                        style={{ height: 80, borderWidth: 0.3, marginTop: 10, width: "100%", borderRadius: 5, fontSize: 14 }}
+                        returnKeyType={'next'}
+                        placeholder="Write Your Reviews"
+                        multiline={true}
+                        keyboardType={'default'}
+                        textAlignVertical={'top'}
+                        onChangeText={(comments) => {
+                          this.setState({ comments })
+                        }
+                        } />
+                    </View>
+                    {errorMsg ?
+                      <Text style={{ color: 'red', marginLeft: 15, marginTop: 5 }}>{errorMsg}</Text> : null}
+                    <Row style={{ marginTop: 20, marginLeft: 14, marginRight: 20 }}>
+
+                      <Col style={{ flexDirection: 'row', width: '45%', alignItems: "flex-start", justifyContent: 'flex-start' }}>
+                        <CheckBox
+                          status={is_anonymous ? true : false}
+                          style={{ borderRadius: 5 }}
+                          checked={this.state.is_anonymous}
+                          onPress={() => { this.setState({ is_anonymous: !is_anonymous }); }}
+                        />
+                        <Text style={{ color: '#3C98EC', marginTop: 10, fontSize: 12 }}>Anonymous</Text>
+                      </Col>
+                    </Row>
+                    <Row style={{ marginLeft: 20, marginTop: 10, marginRight: 20, marginBottom: 20 }}>
+
+                      <Col style={{ width: '50%' }}>
+                      </Col>
+                      <Col style={{ width: '50%', flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+                        <TouchableOpacity style={{ backgroundColor: '#959595', paddingLeft: 20, paddingRight: 20, paddingTop: 3, paddingBottom: 3, borderRadius: 2 }}><Text uppercase={true} style={{ color: '#FFF', fontSize: 12, }} onPress={() => this.submitReview('CANCEL')} >Cancel</Text></TouchableOpacity>
+                        <TouchableOpacity style={{ backgroundColor: '#349631', paddingLeft: 20, paddingRight: 20, paddingTop: 3, paddingBottom: 3, borderRadius: 2, marginLeft: 10 }}><Text uppercase={true} style={{ color: '#FFF', fontSize: 12 }} onPress={() => this.submitReview('ADD')}>Submit</Text></TouchableOpacity>
+                      </Col>
+                    </Row>
                   </View>
                   {errorMsg ?
                     <Text style={{ color: 'red', marginLeft: 15, marginTop: 5 }}>{errorMsg}</Text> : null}
                   <Row style={{ marginTop: 20, marginLeft: 14, marginRight: 20 }}>
 
                     <Col style={{ flexDirection: 'row', width: '45%', alignItems: "flex-start", justifyContent: 'flex-start' }}>
-                       <CheckBox  
-                            status={is_anonymous  ? true : false}
-                            style={{borderRadius:5}}
-                            checked={this.state.is_anonymous}
-                            onPress={() => { this.setState({ is_anonymous: !is_anonymous }); }}
-                                               />
+
+                      <CheckBox
+                        status={is_anonymous ? true : false}
+                        style={{ borderRadius: 5 }}
+                        checked={this.state.is_anonymous}
+                        onPress={() => { this.setState({ is_anonymous: !is_anonymous }); }}
+                      />
                       <Text style={{ color: '#3C98EC', marginTop: 10, fontSize: 12 }}>Anonymous</Text>
                     </Col>
                   </Row>
@@ -370,38 +395,13 @@ render() {
                     </Col>
                   </Row>
                 </View>
-                {errorMsg ?
-                  <Text style={{ color: 'red', marginLeft: 15, marginTop: 5 }}>{errorMsg}</Text> : null}
-                <Row style={{ marginTop: 20, marginLeft: 14, marginRight: 20 }}>
-
-                  <Col style={{ flexDirection: 'row', width: '45%', alignItems: "flex-start", justifyContent: 'flex-start' }}>
-                  
-                      <CheckBox  
-                            status={is_anonymous  ? true : false}
-                            style={{borderRadius:5}}
-                            checked={this.state.is_anonymous}
-                            onPress={() => { this.setState({ is_anonymous: !is_anonymous }); }}
-                                               />
-                    <Text style={{ color: '#3C98EC', marginTop: 10, fontSize: 12 }}>Anonymous</Text>
-                  </Col>
-                </Row>
-                <Row style={{ marginLeft: 20, marginTop: 10, marginRight: 20, marginBottom: 20 }}>
-
-                  <Col style={{ width: '50%' }}>
-                  </Col>
-                  <Col style={{ width: '50%', flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'flex-end' }}>
-                    <TouchableOpacity style={{ backgroundColor: '#959595', paddingLeft: 20, paddingRight: 20, paddingTop: 3, paddingBottom: 3, borderRadius: 2 }}><Text uppercase={true} style={{ color: '#FFF', fontSize: 12, }} onPress={() => this.submitReview('CANCEL')} >Cancel</Text></TouchableOpacity>
-                    <TouchableOpacity style={{ backgroundColor: '#349631', paddingLeft: 20, paddingRight: 20, paddingTop: 3, paddingBottom: 3, borderRadius: 2, marginLeft: 10 }}><Text uppercase={true} style={{ color: '#FFF', fontSize: 12 }} onPress={() => this.submitReview('ADD')}>Submit</Text></TouchableOpacity>
-                  </Col>
-                </Row>
-              </View>
-            </Grid>
-          </Modal>
-        </View>
-      </Content>
-    </Container>
-  )
-}
+              </Grid>
+            </Modal>
+          </View>
+        </Content>
+      </Container>
+    )
+  }
 }
 const styles = StyleSheet.create({
   container:
