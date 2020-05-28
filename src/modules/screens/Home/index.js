4dc976ec2cff0bb5b1286ba5a010720c1497d46f
Bala <balankottiyattil@gmail.com>
2020-05-28 22:18:53 +0530

    implimentation completed

--- src/modules/screens/Home/index.js
+++ src/modules/screens/Home/index.js
@@ -4,7 +4,6 @@ import { logout } from '../../providers/auth/auth.actions';
 import { Col, Row, Grid } from 'react-native-easy-grid';
 import { connect } from 'react-redux'
 import { StyleSheet, Image, View, TouchableOpacity, AsyncStorage, FlatList, ImageBackground, Alert, Linking } from 'react-native';
-
 import { getReferalPoints, getCurrentVersion } from '../../providers/profile/profile.action';
 import { catagries } from '../../providers/catagries/catagries.actions';
 import { MAP_BOX_PUBLIC_TOKEN, IS_ANDROID, MAX_DISTANCE_TO_COVER, CURRENT_PRODUCT_VERSION_CODE } from '../../../setup/config';
@@ -27,10 +26,10 @@ import NotifService from '../../../setup/NotifService';
 import FastImage from 'react-native-fast-image'
 import { translate } from '../../../setup/translator.helper';
 import { authorizeConnectyCube, setUserLoggedIn } from '../VideoConsulation/services/video-consulting-service';
-import { getUserAppointments,getMultipleDoctorDetails } from "../../providers/bookappointment/bookappointment.action";
-import {  getName ,getAllEducation, getAllSpecialist} from '../../common'
-import { formatDate, addTimeUnit,getAllId } from "../../../setup/helpers";
-
+import { getUserAppointments, getMultipleDoctorDetails } from "../../providers/bookappointment/bookappointment.action";
+import { getName, getAllEducation, getAllSpecialist } from '../../common'
+import { formatDate, addTimeUnit, getAllId } from "../../../setup/helpers";
+import moment from 'moment';
 
 class Home extends Component {
 
@@ -42,7 +41,10 @@ class Home extends Component {
             isLoading: false,
             catagary: [],
             categryCount: 0,
-            AppoinmentData:[]
+            AppoinmentData: [],
+            updatedDate: '',
+            AppointmentId: '',
+            doctorInfo: {}
         };
     }
 
@@ -74,7 +76,7 @@ class Home extends Component {
     async componentDidMount() {
         try {
             await this.upCommingNextAppointment(),
-            this.initialFunction();
+                this.initialFunction();
             this._setUpListeners();
             NotifService.initNotification(this.props.navigation);
             if (IS_ANDROID) {
@@ -372,92 +374,105 @@ class Home extends Component {
     };
 
 
-	upCommingNextAppointment = async () => {
-		try {
-       
+    upCommingNextAppointment = async () => {
+        try {
+
             this.setState({ isLoading: true })
-         
+
             let userId = await AsyncStorage.getItem("userId");
-       
-			let filters = {
-				startDate: new Date().toUTCString(),
-				endDate: addTimeUnit(new Date(), 1, "years").toUTCString(),
-				on_going_appointment: true
+
+            let filters = {
+                startDate: new Date().toUTCString(),
+                endDate: addTimeUnit(new Date(), 1, "years").toUTCString(),
+                on_going_appointment: true
             };
-           
-			// alert(JSON.stringify(userId))
-			let upCommingAppointmentResult = await getUserAppointments(userId, filters);
-			console.log('upcomming==================================');
+
+            // alert(JSON.stringify(userId))
+            let upCommingAppointmentResult = await getUserAppointments(userId, filters);
+            console.log('upcomming==================================');
             console.log(upCommingAppointmentResult)
-			if (upCommingAppointmentResult.success) {
-				let doctorInfo = new Map();
-				upCommingAppointmentResult = upCommingAppointmentResult.data;
+            if (upCommingAppointmentResult.success) {
+                let doctorInfo = new Map();
+                upCommingAppointmentResult = upCommingAppointmentResult.data;
 
-				let doctorIds = getAllId(upCommingAppointmentResult)
-				let speciallistResult = await getMultipleDoctorDetails(doctorIds, "specialist,education,prefix,profile_image,gender");
+                let doctorIds = getAllId(upCommingAppointmentResult)
+                let speciallistResult = await getMultipleDoctorDetails(doctorIds, "specialist,education,prefix,profile_image,gender");
 
-				speciallistResult.data.forEach(doctorData => {
-					let educationDetails = ' ';
-					let speaciallistDetails = '';
+                speciallistResult.data.forEach(doctorData => {
+                    let educationDetails = ' ';
+                    let speaciallistDetails = '';
 
-					if (doctorData.education != undefined) {
-						educationDetails = getAllEducation(doctorData.education)
-					}
-					if (doctorData.specialist != undefined) {
-						speaciallistDetails = getAllSpecialist(doctorData.specialist)
-					}
+                    if (doctorData.education != undefined) {
+                        educationDetails = getAllEducation(doctorData.education)
+                    }
+                    if (doctorData.specialist != undefined) {
+                        speaciallistDetails = getAllSpecialist(doctorData.specialist)
+                    }
 
-					doctorInfo.set(doctorData.doctor_id, {
-						degree: educationDetails,
-						specialist: speaciallistDetails,
-						prefix: doctorData.prefix,
-						profile_image: doctorData.profile_image,
-						gender: doctorData.gender
-					})
-				});
+                    doctorInfo.set(doctorData.doctor_id, {
+                        degree: educationDetails,
+                        specialist: speaciallistDetails,
+                        prefix: doctorData.prefix,
+                        profile_image: doctorData.profile_image,
+                        gender: doctorData.gender
+                    })
+                });
 
-				let upcommingInfo = [];
-				upCommingAppointmentResult.map(doctorData => {
-					let details = doctorInfo.get(doctorData.doctor_id)
-					upcommingInfo.push({
-						appointmentResult: doctorData,
-						specialist: details.specialist,
-						degree: details.degree,
-						prefix: details.prefix,
-						profile_image: details.profile_image
-					});
-				})
-				upcommingInfo.sort(function (firstVarlue, secandValue) {
-					return firstVarlue.appointmentResult.appointment_starttime < secandValue.appointmentResult.appointment_starttime ? -1 : 0
-				})
-				this.setState({
-					upComingData: upcommingInfo,
-					AppoinmentData: upcommingInfo,
-					isLoading: false
-				});
-				//  alert(JSON.stringify(this.state.AppoinmentData))
-			}
-		} catch (e) {
-			console.log(e);
-		} finally {
-			this.setState({
-				isLoading: false
-			})
+                let upcommingInfo = [];
+                upCommingAppointmentResult.map(doctorData => {
+                    let details = doctorInfo.get(doctorData.doctor_id)
+                    upcommingInfo.push({
+                        appointmentResult: doctorData,
+                        specialist: details.specialist,
+                        degree: details.degree,
+                        prefix: details.prefix,
+                        profile_image: details.profile_image
+                    });
+                })
+                upcommingInfo.sort(function (firstVarlue, secandValue) {
+                    return firstVarlue.appointmentResult.appointment_starttime < secandValue.appointmentResult.appointment_starttime ? -1 : 0
+                })
 
-		}
-	};
+                let getSingleData = upcommingInfo.slice(0, 1)
+                this.setState({
+                    upComingData: upcommingInfo,
+                    AppoinmentData: getSingleData,
+                    isLoading: false
+                });
 
+                const { AppoinmentData, updatedDate } = this.state
 
+                //alert(JSON.stringify(this.state.AppoinmentData))
+                let time = [...AppoinmentData]
+                time.map((t) => {
+                    let appointmentId = t.appointmentResult._id;
+                    this.setState({ AppointmentId: appointmentId })
+                    let doctorInfo = t.appointmentResult.doctorInfo
+                    this.setState({ doctorInfo: doctorInfo })
+                    let dateData = formatDate(t.appointmentResult.appointment_starttime, "dddd,MMMM DD-YYYY")
+                    let currentDate = formatDate(new Date().toISOString(), "dddd,MMMM DD-YYYY")
+                    var dateServerToHandleTomorrow = new Date(currentDate);
+                    var tomorrow = new Date(dateServerToHandleTomorrow.setDate(dateServerToHandleTomorrow.getDate() + 1));
+                    let tomorrowDate = formatDate(tomorrow.toISOString(), "dddd,MMMM DD-YYYY")
+                    if (currentDate == dateData) {
+                        console.log("currentDate")
+                        this.setState({ updatedDate: "Today" })
+                    } else if (tomorrowDate == dateData) {
+                        console.log("tomorrowDate")
+                        this.setState({ updatedDate: "Tomorrow" })
+                    } else {
+                        console.log("how")
+                        this.setState({ updatedDate: dateData })
+                    }
+                })
 
-
-
-
-
-
-
-
-
-
+            }
+        } catch (e) {
+            console.log(e);
+        } finally {
+            this.setState({ isLoading: false });
+        }
+    };
 
 
     render() {
@@ -473,7 +488,7 @@ class Home extends Component {
             this.locationUpdatedCount = locationUpdatedCount;
 
         }
-        const {AppoinmentData} = this.state
+        const { AppoinmentData, updatedDate, AppointmentId, doctorInfo } = this.state
         return (
 
             <Container style={styles.container}>
@@ -482,22 +497,22 @@ class Home extends Component {
                     <NavigationEvents
                         onWillFocus={payload => { this.backNavigation(payload) }}
                     />
-                        <Row style={styles.SearchRow}>
-                            <Col size={0.9} style={styles.SearchStyle}>
-                                <Icon name="ios-search" style={{ color: '#fff', fontSize: 20, padding: 2 }} />
-                            </Col>
-                            <Col size={8.1} style={{ justifyContent: 'center', }}>
-                                <Input
-                                    onFocus={() => { this.props.navigation.navigate("RenderSuggestionList")}}
-                                    placeholder="Search for Symptoms/Services,etc"
-                                    style={styles.inputfield}
-                                    placeholderTextColor="#e2e2e2"
-                                    editable={true}
-                                    underlineColorAndroid="transparent"
-                                />
-                            </Col>
-                        </Row>
-                   
+                    <Row style={styles.SearchRow}>
+                        <Col size={0.9} style={styles.SearchStyle}>
+                            <Icon name="ios-search" style={{ color: '#fff', fontSize: 20, padding: 2 }} />
+                        </Col>
+                        <Col size={8.1} style={{ justifyContent: 'center', }}>
+                            <Input
+                                onFocus={() => { this.props.navigation.navigate("RenderSuggestionList") }}
+                                placeholder="Search for Symptoms/Services,etc"
+                                style={styles.inputfield}
+                                placeholderTextColor="#e2e2e2"
+                                editable={true}
+                                underlineColorAndroid="transparent"
+                            />
+                        </Col>
+                    </Row>
+
                     <Grid style={{ flex: 1, marginLeft: 10, marginRight: 20, marginTop: 10 }}>
                         <Col style={{ width: '33%', }}>
                             <TouchableOpacity onPress={() =>
@@ -685,44 +700,44 @@ class Home extends Component {
                         </View>
                         <Row style={{ marginTop: 10, marginBottom: 5 }}>
                             <Left>
-                                <Text style={styles.mainHead}>You have an Appointment Today</Text>
+                                <Text style={styles.mainHead}>You have an Appointment on {updatedDate}</Text>
                             </Left>
                         </Row>
-                        <Card style={{  }}>
-                                <TouchableOpacity onPress={() => this.props.navigation.navigate("EarnReward")}>
-                                    <Row style={{ height: 30, width: '100%', overflow: 'hidden', backgroundColor: "#8EC63F", }}>
-                                        <Col style={{ width: '90%',justifyContent:'center' }}>
-                                            <Text style={{
-                                                  color: '#fff',
-                                                  fontSize: 14,
-                                                  lineHeight: 20,
-                                                  marginLeft:15,
-                                                  fontWeight: '500',
-                                            }}>Your Appointment Schedule</Text>
-                                           </Col>
-                                           <Col style={{ width: '10%',justifyContent:'center' }}>
-                                               <Icon name="ios-information-circle-outline" style={{color:'#fff',fontSize:25}}/>
-                                           </Col>
-                                    </Row>
-                                    <FlatList
-										data={AppoinmentData}
-										extraData={AppoinmentData}
-										renderItem={({ item, index }) => (
-                                    <Row style={{ height: 60, width: '100%', overflow: 'hidden', backgroundColor: "#fff",marginBottom:10,marginTop:10  }}>
-                                        <Col style={{ width: '100%',justifyContent:'center',}}>
-                                            <Text style={{
-                                                  color: 'gray',
-                                                  fontSize: 14,
-                                                  lineHeight: 20,
-                                                  marginLeft:15,
-                                                  marginRight:15,
-                                                  fontWeight: '500',
-                                            }}>You  have  an  Appointment  today  with  Dr.Balasubramanian  	{(item.prefix != undefined ? item.prefix + ' ' : '') + getName(item.appointmentResult.doctorInfo)}  and  is  scheduled  at  4:00 pm.Please  prepare  for  the  Appointment</Text>
-                                           </Col>
-                                    </Row>
-                                        )}/>
-                                </TouchableOpacity>
-                            </Card>
+                        <Card style={{}}>
+                            <TouchableOpacity onPress={() => { this.props.navigation.navigate('PrepareAppointmentWizard', { AppointmentId: AppointmentId, DoctorData: AppoinmentData, Data: doctorInfo }) }}>
+                                <Row style={{ height: 30, width: '100%', overflow: 'hidden', backgroundColor: "#8EC63F", }}>
+                                    <Col style={{ width: '90%', justifyContent: 'center' }}>
+                                        <Text style={{
+                                            color: '#fff',
+                                            fontSize: 14,
+                                            lineHeight: 20,
+                                            marginLeft: 15,
+                                            fontWeight: '500',
+                                        }}>Your Appointment Schedule</Text>
+                                    </Col>
+                                    <Col style={{ width: '10%', justifyContent: 'center' }}>
+                                        <Icon name="ios-information-circle-outline" style={{ color: '#fff', fontSize: 25 }} />
+                                    </Col>
+                                </Row>
+                                <FlatList
+                                    data={AppoinmentData}
+                                    extraData={AppoinmentData}
+                                    renderItem={({ item, index }) => (
+                                        <Row style={{ width: '100%', overflow: 'hidden', backgroundColor: "#fff", marginBottom: 10, marginTop: 10 }}>
+                                            <Col style={{ width: '100%', justifyContent: 'center', }}>
+                                                <Text style={{
+                                                    color: 'gray',
+                                                    fontSize: 14,
+                                                    lineHeight: 20,
+                                                    marginLeft: 15,
+                                                    marginRight: 15,
+                                                    fontWeight: '500',
+                                                }}>You  have  an  Appointment   with   {(item.prefix != undefined ? item.prefix + ' ' : '') + getName(item.appointmentResult.doctorInfo)}  and  is  scheduled  at {formatDate(item.appointmentResult.appointment_starttime, "hh:mm a")}.Please  prepare  for  the  Appointment</Text>
+                                            </Col>
+                                        </Row>
+                                    )} />
+                            </TouchableOpacity>
+                        </Card>
                     </View>
 
                 </Content>
