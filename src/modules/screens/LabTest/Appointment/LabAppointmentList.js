0fb46f63a5e175f743ce9ebb4f825348dd3ab553
Shobana <shobanarathnakumar@gmail.com>
2020-07-15 00:56:47 +0530

    MEDAP-2679:Lab Book Again

--- src/modules/screens/LabTest/Appointment/LabAppointmentList.js
+++ src/modules/screens/LabTest/Appointment/LabAppointmentList.js
@@ -258,7 +258,7 @@ class LabAppointmentList extends Component {
                                     }}>No appoinments are scheduled
 								</Text>
                                     <Item style={{ marginTop: "15%", borderBottomWidth: 0 }}>
-                                        <Button style={[styles.bookingButton, styles.customButton]} onPress={() => this.props.navigation.navigate("Home")
+                                        <Button style={[styles.bookingButton, styles.customButton]} onPress={() => this.props.navigation.navigate("Lab Test")
                                         } testID='navigateToHome'>
                                             <Text style={{ fontFamily: 'Opensans', fontSize: 15, fontWeight: 'bold' }}>Book Now</Text>
                                         </Button>
