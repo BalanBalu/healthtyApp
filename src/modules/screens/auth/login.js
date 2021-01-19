860ab5fac187cf68587c5c64dbe7c8dbf6b783e3
Nivas <tharakprofessional@gmail.com>
2021-01-19 15:37:04 +0530

    selectedTypechanges

--- src/modules/screens/auth/login.js
+++ src/modules/screens/auth/login.js
@@ -26,7 +26,7 @@ class Login extends Component {
       checked: false,
       isModalVisible: false,
       showPassword: true,
-      isSelected: 'corporate_user',
+      isSelected:CURRENT_APP_NAME=== MY_SMART_HEALTH_CARE?'corporate_user':'user',
       CorporateUser: false
     }
   }
