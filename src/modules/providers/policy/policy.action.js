cd6c0e29e54692d8ccde8942f36870322491c0b6
Shobana <shobanarathnakumar@gmail.com>
2021-01-28 17:41:43 +0530

    Policy coverage implemetation

---
+++ src/modules/providers/policy/policy.action.js
@@ -0,0 +1,16 @@
+import { smartHealthGetService, smartHealthPostService, smartHealthPutService } from '../../../setup/services/httpservices';
+export async function getPolicyByPolicyNo(policyNo) {
+    try {
+        let endPoint = '/policy/by-policyNo?pno=' + policyNo;
+        let response = await smartHealthGetService(endPoint);
+        let respData = response.data;
+        return respData;
+
+    } catch (e) {
+        return {
+            message: 'exception' + e,
+            success: false
+        }
+    }
+}
+
