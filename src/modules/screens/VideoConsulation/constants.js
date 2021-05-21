672ed4cc0e9796115daab5d605bca213fb3fea50
Hari Krishnan <tharakprofessional@gmail.com>
2021-05-21 17:46:06 +0530

    multilangDefectfix

--- src/modules/screens/VideoConsulation/constants.js
+++ src/modules/screens/VideoConsulation/constants.js
@@ -1,61 +1,59 @@
-import {translate} from '../../../setup/translator.helper';
-
 export const POSSIBLE_VIDEO_CONSULTING_STATUS = {
-    PAYMENT_IN_PROGRESS :translate('PAYMENT_IN_PROGRESS'),
-    FAILED: translate('FAILED'),
-    PENDING: translate('PENDING'),
-    APPROVED: translate('APPROVED'),
-    CANCELED: translate('CANCELED'),
-    CLOSED: translate('CLOSED')
+    PAYMENT_IN_PROGRESS :'PAYMENT_IN_PROGRESS',
+    FAILED: 'FAILED',
+    PENDING:'PENDING',
+    APPROVED: 'APPROVED',
+    CANCELED: 'CANCELED',
+    CLOSED: 'CLOSED'
   }
 
   export const STATUS_VALUE_DATA = {
     PENDING: {
-      text: translate("Waiting for the Doctor's Approval"),
-      statusText: translate('Pending'),
+      text: "Waiting for the Doctor's Approval",
+      statusText:'Pending',
       color: 'red',
       icon: 'ios-information-circle-outline',
       type: 'DOCTOR_REPORT_ISSUE_APPOINTMENT_PENDING'
     },
     APPROVED: {
-      text: translate('Video Consultation Confirmed'),
-      statusText: translate('Approved'),
+      text:'Video Consultation Confirmed',
+      statusText:'Approved',
       color: 'green',
       icon: 'checkmark-circle',
       type: 'DOCTOR_REPORT_ISSUE_APPOINTMENT_APPROVED',
     },
     COMPLETED: {
-      text: translate('Video Consultation Resolved/ Completed'),
-      statusText: translate('Completed'),
+      text:'Video Consultation Resolved/ Completed',
+      statusText:'Completed',
       color: 'green',
       icon: 'checkmark-circle',
       type: 'DOCTOR_REPORT_ISSUE_APPOINTMENT_COMPLETED'
     },
     CLOSED: {
-      text: translate('No Response'),
-      statusText: translate('Closed'),
+      text: 'No Response',
+      statusText:'Closed',
       color: '#d43640',
       icon: 'ios-close-circle',
       type: 'DOCTOR_REPORT_ISSUE_APPOINTMENT_CANCELED'
       // icon: 'closecircleo'
     },
     CANCELED: {
-      text: translate('Video Consultation Cancelled'),
-      statusText: translate('Canceled'),
+      text:'Video Consultation Cancelled',
+      statusText:'Canceled',
       color: '#d43640',
       icon: 'ios-close-circle',
       type: 'DOCTOR_REPORT_ISSUE_APPOINTMENT_CANCELED'
     },
     REJECTED: {
-      text: translate('Video Consultation Rejected'),
-      statusText: translate('Rejected'),
+      text:'Video Consultation Rejected',
+      statusText:'Rejected',
       color: '#d43640',
       icon: 'ios-close-circle',
       type: 'DOCTOR_REPORT_ISSUE_APPOINTMENT_CANCELED'
     },
     PAYMENT_IN_PROGRESS: {
-        text: translate('Payment is not yet completed'),
-        statusText: translate('Payment In Progress'),
+        text: 'Payment is not yet completed',
+        statusText:'Payment In Progress',
         color: '#d43640',
         icon: 'ios-sync',
         type: 'DOCTOR_REPORT_ISSUE_APPOINTMENT_CANCELED'
