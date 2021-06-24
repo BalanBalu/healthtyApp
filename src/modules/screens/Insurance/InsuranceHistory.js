4b202f5544923d756dc7f80d998ad5fe98499211
HariKrishnan <tharakprofessional@gmail.com>
2021-06-24 17:00:06 +0530

    Loader

--- src/modules/screens/Insurance/InsuranceHistory.js
+++ src/modules/screens/Insurance/InsuranceHistory.js
@@ -1,14 +1,13 @@
-import React, {Component} from 'react';
-import {Container, Content, Toast, Text, Item, Card} from 'native-base';
-import {Col, Row} from 'react-native-easy-grid';
-import {View, FlatList, ActivityIndicator, StyleSheet} from 'react-native';
-import {primaryColor} from '../../../setup/config';
+import React, { Component } from 'react';
+import { Container, Content, Toast, Text, Item, Card } from 'native-base';
+import { Col, Row } from 'react-native-easy-grid';
+import { View, FlatList, ActivityIndicator, StyleSheet } from 'react-native';
+import { primaryColor } from '../../../setup/config';
 import SegmentedControlTab from 'react-native-segmented-control-tab';
-import {getInsuranceHistory} from '../../providers/insurance/insurance.action';
+import { getInsuranceHistory } from '../../providers/insurance/insurance.action';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Loader } from '../../../components/ContentLoader';
-import {dateDiff, formatDate} from '../../../setup/helpers';
-
+import { dateDiff, formatDate } from '../../../setup/helpers';
 
 const LIMIT = 5;
 
@@ -17,6 +16,9 @@ export default class InsuranceHistory extends Component {
     super();
     this.state = {
       selectedIndex: 0,
+      isLoading: true,
+      isLoadingMoreData: true,
+      isLoadingBuyInsurance: true
     };
     this.isEnabledLoadMoreData = true;
     this.isEnabledLoadMoreInsuranceData = true;
@@ -37,16 +39,18 @@ export default class InsuranceHistory extends Component {
     });
     if (index == 1) {
       this.getBuyInsuranceList();
-    }else{
+      // this.setState({ isLoading: true });
+    } else {
       this.getInsuranceList();
+      // this.setState({ isLoading: true });
     }
   };
   getInsuranceList = async (actionType) => {
     try {
       let memberId = await AsyncStorage.getItem('memberId');
       let result = await getInsuranceHistory(
-       memberId,
-       'INSURANCE',
+        memberId,
+        'INSURANCE',
         this.pagination,
         LIMIT,
       );
@@ -54,7 +58,7 @@ export default class InsuranceHistory extends Component {
       if (result && result.docs && result.docs.length) {
         this.pagination = this.pagination + 1;
         this.insuranceData = [...this.insuranceData, ...result.docs];
-        this.setState({isLoading: false, data: this.insuranceData});
+        this.setState({ isLoading: false, data: this.insuranceData });
       } else {
         if (this.insuranceData.length > 3) {
           toastMeassage('No more data Available!', 'success', 3000);
@@ -64,11 +68,9 @@ export default class InsuranceHistory extends Component {
     } catch (e) {
       console.log(e);
     } finally {
-      this.setState({isLoading: false});
+      this.setState({ isLoading: false });
     }
   };
-
-  
   getBuyInsuranceList = async (actionType) => {
     try {
       let memberId = await AsyncStorage.getItem('memberId');
@@ -81,7 +83,7 @@ export default class InsuranceHistory extends Component {
       if (result && result.docs && result.docs.length) {
         this.pagination1 = this.pagination1 + 1;
         this.buyInsuranceData = [...this.buyInsuranceData, ...result.docs];
-        this.setState({isLoading: false, buyInsuranceData: this.buyInsuranceData});
+        this.setState({ isLoadingBuyInsurance: false, isLoading: false, buyInsuranceData: this.buyInsuranceData });
 
       } else {
         if (this.buyInsuranceData.length > 3) {
@@ -92,34 +94,34 @@ export default class InsuranceHistory extends Component {
     } catch (e) {
       console.log(e);
     } finally {
-      this.setState({isLoading: false});
+      this.setState({ isLoadingBuyInsurance: false, isLoading: false });
     }
   };
   loadMoreData = async () => {
     try {
-      this.setState({isLoadingMoreData: true});
+      this.setState({ isLoadingMoreData: true });
       await this.getInsuranceList();
     } catch (error) {
       console.log('Ex is getting on load more data', error.message);
     } finally {
-      this.setState({isLoadingMoreData: false});
+      this.setState({ isLoadingMoreData: false });
     }
   };
 
   loadMoreBuyInsuranceData = async () => {
     try {
-      this.setState({isLoadingMoreData: true});
-      await this.getInsuranceList();
+      this.setState({ isLoadingMoreData: true });
+      await this.getBuyInsuranceList();
     } catch (error) {
       console.log('Ex is getting on load more data', error.message);
     } finally {
-      this.setState({isLoadingMoreData: false});
+      this.setState({ isLoadingMoreData: false });
     }
   };
 
 
   render() {
-    const {selectedIndex,data,buyInsuranceData,isLoading} = this.state;
+    const { selectedIndex, data, buyInsuranceData, isLoading, isLoadingBuyInsurance } = this.state;
     return (
       <Container>
         <Content>
@@ -141,172 +143,180 @@ export default class InsuranceHistory extends Component {
                   borderColor: primaryColor,
                   fontFamily: 'Roboto',
                 }}
-                tabStyle={{borderColor: primaryColor, fontFamily: 'Roboto'}}
+                tabStyle={{ borderColor: primaryColor, fontFamily: 'Roboto' }}
               />
             </Card>
             {selectedIndex === 0 ? (
-               isLoading ? (
-                <Loader style="list" />
-              ) : data&&data.length ? (
-              <View>
-                 <FlatList
-                data={data}
-                keyExtractor={(item, index) => index.toString()}
-                onEndReachedThreshold={0.5}
-                onEndReached={() => {
-                  if (this.isEnabledLoadMoreInsuranceData) {
-                    this.loadMoreData();
-                  }
-                }}
-                renderItem={({item, index}) => (
-                <Card style={styles.cardStyle}>
-                  <Col>
-                    <Row>
-                      <Text
-                        style={{
-                          fontFamily: 'opensans-bold',
-                          fontSize: 14,
-                          color: 'primaryColor',
-                          marginLeft: 5,
-                        }}>
-                        Renewal Date
-                      </Text>
-                      <Text style={{marginLeft: 30}}>:</Text>
-                      <Text style={{marginLeft: 11}}>{formatDate(item.renewalDate, 'DD-MM-YY')}</Text>
-                    </Row>
-                  </Col>
-
-                  <Col>
-                    <Row>
-                      <Text
-                        style={{
-                          fontSize: 15,
-                          marginLeft: 5,
-                          fontFamily: 'Roboto',
-                        }}>
-                        PolicyType
-                      </Text>
-                      <Text style={{marginLeft: 50}}>:</Text>
-                      <Text style={{marginLeft: 11}}>{item.policyType}</Text>
-                    </Row>
-                  </Col>
-
-                  <Col>
-                    <Row>
-                      <Text
-                        style={{
-                          fontSize: 15,
-                          marginLeft: 5,
-                          fontFamily: 'Roboto',
-                        }}>
-                        TransactionType
-                      </Text>
-                      <Text style={{marginLeft: 11}}>:</Text>
-                      <Text style={{marginLeft: 11}}>{item.transactionType}</Text>
-                    </Row>
-                  </Col>
-                </Card>
-                 )}
-                 />
-              </View>): (
-            <Item
-              style={{
-                borderBottomWidth: 0,
-                marginTop: 100,
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Text
-                style={{
-                  fontSize: 20,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                {' '}
-                No insurance policy list found!
-              </Text>
-            </Item>
-          )
-            ) : selectedIndex === 1 ? (
               isLoading ? (
-                <Loader style="list" />
-              ) : buyInsuranceData&&buyInsuranceData.length ? (
-              <FlatList
-              data={buyInsuranceData}
-              keyExtractor={(item, index) => index.toString()}
-              onEndReachedThreshold={0.5}
-              onEndReached={() => {
-                if (this.isEnabledLoadMoreData) {
-                  this.loadMoreBuyInsuranceData();
-                }
-              }}
-              renderItem={({item, index}) => (
-              <Card style={styles.cardStyle}>
-                <Col>
-                  <Row>
-                    <Text
-                      style={{
-                        fontSize: 14,
-                        color: 'primaryColor',
-                        marginLeft: 5,
-                        fontFamily: 'opensans-bold',
-                      }}>
-                      Request Date
-                    </Text>
-                    <Text style={{marginLeft: 32}}>:</Text>
-                    <Text style={{marginLeft: 11}}>{formatDate(item.requestedDate, 'DD-MM-YY')}</Text>
-                  </Row>
-                </Col>
+                <Loader style="newList" />
+              ) : data && data.length ? (
+                <View>
+                  <FlatList
+                    data={data}
+                    keyExtractor={(item, index) => index.toString()}
+                    onEndReachedThreshold={0.5}
+                    onEndReached={() => {
+                      if (this.isEnabledLoadMoreInsuranceData) {
+                        this.loadMoreData();
+                      }
+                    }}
+                    renderItem={({ item, index }) => (
+                      <Card style={styles.cardStyle}>
+                        <Col>
+                          <Row>
+                            <Text
+                              style={{
+                                fontFamily: 'opensans-bold',
+                                fontSize: 14,
+                                color: 'primaryColor',
+                                marginLeft: 5,
+                              }}>
+                              Renewal Date
+                            </Text>
+                            <Text style={{ marginLeft: 30 }}>:</Text>
+                            <Text style={{ marginLeft: 11 }}>{formatDate(item.renewalDate, 'DD-MM-YY')}</Text>
+                          </Row>
+                        </Col>
 
-                <Col>
-                  <Row>
-                    <Text
-                      style={{
-                        fontSize: 15,
-                        marginLeft: 5,
-                        fontFamily: 'Roboto',
-                      }}>
-                      PolicyType
-                    </Text>
-                    <Text style={{marginLeft: 50}}>:</Text>
-                    <Text style={{marginLeft: 11}}>{item.policyType}</Text>
-                  </Row>
-                </Col>
+                        <Col>
+                          <Row>
+                            <Text
+                              style={{
+                                fontSize: 15,
+                                marginLeft: 5,
+                                fontFamily: 'Roboto',
+                              }}>
+                              PolicyType
+                            </Text>
+                            <Text style={{ marginLeft: 50 }}>:</Text>
+                            <Text style={{ marginLeft: 11 }}>{item.policyType}</Text>
+                          </Row>
+                        </Col>
 
-                <Col>
-                  <Row>
-                    <Text
-                      style={{
-                        fontSize: 15,
-                        marginLeft: 5,
-                        fontFamily: 'Roboto',
-                      }}>
-                      TransactionType
-                    </Text>
-                    <Text style={{marginLeft: 11}}>:</Text>
-                    <Text style={{marginLeft: 11}}>{item.transactionType}</Text>
-                  </Row>
-                </Col>
-              </Card>
-              )}/>
-            ): (
-              <Item
-                style={{
-                  borderBottomWidth: 0,
-                  marginTop: 100,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}>
-                <Text
+                        <Col>
+                          <Row>
+                            <Text
+                              style={{
+                                fontSize: 15,
+                                marginLeft: 5,
+                                fontFamily: 'Roboto',
+                              }}>
+                              TransactionType
+                            </Text>
+                            <Text style={{ marginLeft: 11 }}>:</Text>
+                            <Text style={{ marginLeft: 11 }}>{item.transactionType}</Text>
+                          </Row>
+                        </Col>
+                      </Card>
+                    )}
+                  />
+                </View>) : (
+                <Item
                   style={{
-                    fontSize: 20,
+                    borderBottomWidth: 0,
+                    marginTop: 100,
                     justifyContent: 'center',
                     alignItems: 'center',
                   }}>
-                  {' '}
-                  No insurance policy list found!
-                </Text>
-              </Item>) ): null}
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    {' '}
+                    No insurance policy list found!
+                  </Text>
+                </Item>
+              )
+            ) : selectedIndex === 1 ? (
+              isLoadingBuyInsurance ? (
+                <Loader style="newList" />
+              ) : buyInsuranceData && buyInsuranceData.length ? (
+                <FlatList
+                  data={buyInsuranceData}
+                  keyExtractor={(item, index) => index.toString()}
+                  onEndReachedThreshold={0.5}
+                  onEndReached={() => {
+                    if (this.isEnabledLoadMoreData) {
+                      this.loadMoreBuyInsuranceData();
+                    }
+                  }}
+                  renderItem={({ item, index }) => (
+                    <Card style={styles.cardStyle}>
+                      <Col>
+                        <Row>
+                          <Text
+                            style={{
+                              fontSize: 14,
+                              color: 'primaryColor',
+                              marginLeft: 5,
+                              fontFamily: 'opensans-bold',
+                            }}>
+                            Request Date
+                          </Text>
+                          <Text style={{ marginLeft: 32 }}>:</Text>
+                          <Text style={{ marginLeft: 11 }}>{formatDate(item.requestedDate, 'DD-MM-YY')}</Text>
+                        </Row>
+                      </Col>
+
+                      <Col>
+                        <Row>
+                          <Text
+                            style={{
+                              fontSize: 15,
+                              marginLeft: 5,
+                              fontFamily: 'Roboto',
+                            }}>
+                            PolicyType
+                          </Text>
+                          <Text style={{ marginLeft: 50 }}>:</Text>
+                          <Text style={{ marginLeft: 11 }}>{item.policyType}</Text>
+                        </Row>
+                      </Col>
+
+                      <Col>
+                        <Row>
+                          <Text
+                            style={{
+                              fontSize: 15,
+                              marginLeft: 5,
+                              fontFamily: 'Roboto',
+                            }}>
+                            TransactionType
+                          </Text>
+                          <Text style={{ marginLeft: 11 }}>:</Text>
+                          <Text style={{ marginLeft: 11 }}>{item.transactionType}</Text>
+                        </Row>
+                      </Col>
+                    </Card>
+                  )} />
+              ) : (
+                <Item
+                  style={{
+                    borderBottomWidth: 0,
+                    marginTop: 100,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                    }}>
+                    {' '}
+                    No insurance policy list found!
+                  </Text>
+                </Item>)) : null}
+            <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+              {this.state.isLoadingMoreData ? <ActivityIndicator
+                style={{ marginBottom: 17 }}
+                animating={this.state.isLoadingMoreData}
+                size="large"
+                color='blue'
+              /> : null}
+            </View>
           </View>
         </Content>
       </Container>
