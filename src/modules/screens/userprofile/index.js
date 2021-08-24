537aa09283ecb7f113ec96a3752e5d2e7580786d
HariKrishnan <ajayduos@gmail.com>
2021-08-24 15:56:24 +0530

    fix

--- src/modules/screens/userprofile/index.js
+++ src/modules/screens/userprofile/index.js
@@ -983,19 +983,19 @@ class Profile extends Component {
                         <Text note style={styles.customText1}>
                           {data?.address1 + ','}
                           <Text note style={styles.customText1}>
-                            {data?.address2 ? data?.address2 : ' '}
+                            {data?.address2 ? data?.address2 +',': ''}
                           </Text>
                         </Text>
                         <Text note style={styles.customText1}>
-                          {data?.address3 ? data?.address3 + ',' : ' '}
+                          {data?.address3 ? data?.address3 + ',' : ''}
                           <Text note style={styles.customText1}>
-                            {data?.city ? data?.city : ' '}
+                            {data?.city ? data?.city : ''}
                           </Text>
                         </Text>
                         <Text note style={styles.customText1}>
-                          {data?.state ? data?.state + ',' : ' '}
+                          {data?.state ? data?.state + ',' : ''}
                           <Text note style={styles.customText1}>
-                            {data?.country ? data?.country : ' '}
+                            {data?.country ? data?.country : ''}
                           </Text>
                         </Text>
                         <Text note style={styles.customText1}>
